456b597e9fd1cd9000f85a7684fc7452fc16ea3d
Fred Lefevere-Laoide <Fred.Lefevere-Laoide@Avaiga.com>
2021-09-15 13:46:39 +0200

    webpack simple generation: - npm run build - npm run build:dev flatly generated into taipy_webapp hence the simplication of server.py

--- gui/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Generate the React production build and copy it to the top directory
-npm run build
-
-cd ../
-rm -rf taipy_webapp
-cp -R gui/build taipy_webapp
-mv taipy_webapp/static/* taipy_webapp
-rmdir taipy_webapp/static
