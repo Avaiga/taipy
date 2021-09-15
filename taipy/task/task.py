44b1ad895d4df4b533944b73eefb1aefb884a478
jrobinAV <88036007+jrobinAV@users.noreply.github.com>
2021-09-15 14:20:11 +0200

    change signature of task to be able to create a task with a single input or a single output

--- taipy/task/task.py
+++ taipy/task/task.py
@@ -1,15 +1,21 @@
-from importlib import import_module
-from typing import List
+from typing import List, Union
 
 from taipy.data.data_source import DataSource
 
 
 class Task:
 
-    def __init__(self, name: str, inputs: List[DataSource], function, outputs: List[DataSource] = None):
-        if outputs is None:
-            outputs = []
-        self.input_data_sources = inputs
-        self.output_data_sources = outputs
+    def __init__(self, name: str,
+                 inputs: Union[DataSource, List[DataSource]],
+                 function,
+                 outputs:  Union[DataSource, List[DataSource]]):
+        if isinstance(inputs, DataSource):
+            self.inputs = List[inputs]
+        else:
+            self.inputs = inputs
+        if isinstance(outputs, DataSource):
+            self.outputs = List[outputs]
+        else:
+            self.outputs = outputs
         self.name = name
         self.function = function
