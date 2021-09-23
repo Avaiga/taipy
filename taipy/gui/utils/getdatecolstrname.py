ae69d851bc8b13e4d88909dd79e7d85edc3a86dd
Fred Lefevere-Laoide <Fred.Lefevere-Laoide@Avaiga.com>
2021-09-23 16:28:45 +0200

    mypy

--- taipy/gui/utils/getdatecolstrname.py
+++ taipy/gui/utils/getdatecolstrname.py
@@ -4,5 +4,5 @@ import pandas as pd
 def get_date_col_str_name(df: pd.DataFrame, col: str):
     sufix = "_str"
     while col + sufix in df.columns:
-        sufix = +"_"
+        sufix = sufix + "_"
     return col + sufix
