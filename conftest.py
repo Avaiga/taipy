57c73b7a28f11d85fd90dc5b56de51c6e42fff91
João André <joao.dasilva@avaiga.com>
2021-09-27 07:08:47 -0300

    Update csv content on write function

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import pytest
+
+
+@pytest.fixture(scope="function")
+def csv_file(tmpdir_factory):
+    csv = pd.DataFrame([{"a": 1, "b": 2, "c": 3}, {"a": 4, "b": 5, "c": 6}])
+    fn = tmpdir_factory.mktemp("data").join("df.csv")
+    csv.to_csv(str(fn), index=False)
+    return fn
+
+
+@pytest.fixture(scope="session")
+def default_data_frame():
+    return pd.DataFrame([{"a": 1, "b": 2, "c": 3}, {"a": 4, "b": 5, "c": 6}])
