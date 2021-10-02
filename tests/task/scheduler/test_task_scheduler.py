fcbe027ed56fd19202b87c3402cef97fedb22a30
Vuillemot Florian <florian.vuillemot@posteo.net>
2021-10-02 19:08:25 +0200

    Revert "Update multi threading task scheduler to multi processing (#109)" (#153)
    This reverts commit d9ffff1f3c4c26500b88889a87fb51726f399c10.

--- tests/task/scheduler/test_task_scheduler.py
+++ tests/task/scheduler/test_task_scheduler.py
@@ -1,7 +1,7 @@
-import multiprocessing
 import uuid
-from functools import partial
+from concurrent.futures import Future
 from time import sleep
+from typing import Optional
 
 from taipy.data.entity import EmbeddedDataSourceEntity
 from taipy.data.scope import Scope
@@ -13,9 +13,26 @@ def mult(nb1: float, nb2: float):
     return nb1 * nb2
 
 
-def lock_mult(lock, nb1: float, nb2: float):
-    with lock:
-        return mult(nb1, nb2)
+class WaitingMult:
+    """
+    This class emulates a time-consuming calculation task
+    The TaskScheduler will call the `__call__` function which will block until the main
+    thread calls the `unblock` method
+    """
+
+    def __init__(self):
+        self._nb1: Optional[float] = None
+        self._nb2: Optional[float] = None
+        self.future = Future()
+
+    def __call__(self, nb1: float, nb2: float):
+        self._nb1 = nb1
+        self._nb2 = nb2
+        return self.future.result()
+
+    def unblock(self):
+        self.future.set_result(self._nb1 * self._nb2)
+        sleep(0.1)  # Wait end of callback
 
 
 def test_scheduled_task():
@@ -26,96 +43,50 @@ def test_scheduled_task():
     assert task.output[0].get(None) == 42
 
 
-def test_scheduled_task_that_return_multiple_outputs():
-    def return_tuple(nb1, nb2):
-        return mult(nb1, nb2), mult(nb1, nb2) / 2
-
-    def return_list(nb1, nb2):
-        return [mult(nb1, nb2), mult(nb1, nb2) / 2]
-
-    task_scheduler = TaskScheduler()
-
-    with_tuple = _create_task(return_tuple, 2)
-    with_list = _create_task(return_list, 2)
-
-    task_scheduler.submit(with_tuple)
-    task_scheduler.submit(with_list)
-
-    assert with_tuple.output[0].get(None) == with_list.output[0].get(None) == 42
-    assert with_tuple.output[1].get(None) == with_list.output[1].get(None) == 21
-
-
-def test_un_writing_data_source_due_difference_between_nb_results_and_nb_data_sources():
-    task_scheduler = TaskScheduler()
-    task = _create_task(lambda nb1, nb2: (mult(nb1, nb2), mult(nb1, nb2) / 2))
-
-    task_scheduler.submit(task)
-    assert task.output[0].get(None) == 0
-
-
-def test_error_during_writing_data_source_dont_stop_writing_on_other_data_source():
-    task_scheduler = TaskScheduler()
-
-    task = _create_task(lambda nb1, nb2: (42, 21), 2)
-    task.output[0].write = None
-    task_scheduler.submit(task)
-
-    assert task.output[0].get(None) == 0
-    assert task.output[1].get(None) == 21
-
-
-def test_scheduled_task_in_parallel():
+def test_scheduled_task_multithreading():
     task_scheduler = TaskScheduler(parallel_execution=True)
-    m = multiprocessing.Manager()
-    lock = m.Lock()
+    sleep_mult = WaitingMult()
+    task = _create_task(sleep_mult)
 
-    task = _create_task(partial(lock_mult, lock))
+    task_scheduler.submit(task)
+    assert task.output[0].get(None) == 0
 
-    with lock:
-        task_scheduler.submit(task)
-        assert task.output[0].get(None) == 0
-
-    sleep(1)
+    sleep_mult.unblock()
     assert task.output[0].get(None) == 42
 
 
-def test_scheduled_task_in_parallel_on_multiple_task():
+def test_scheduled_task_multithreading_multiple_task():
     task_scheduler = TaskScheduler(parallel_execution=True)
 
-    m = multiprocessing.Manager()
-    lock_1 = m.Lock()
-    lock_2 = m.Lock()
+    sleep_mult_1 = WaitingMult()
+    sleep_mult_2 = WaitingMult()
 
-    task_1 = _create_task(partial(lock_mult, lock_1))
-    task_2 = _create_task(partial(lock_mult, lock_2))
+    task_1 = _create_task(sleep_mult_1)
+    task_scheduler.submit(task_1)
 
-    with lock_1:
-        with lock_2:
-            task_scheduler.submit(task_1)
-            task_scheduler.submit(task_2)
+    task_2 = _create_task(sleep_mult_2)
+    task_scheduler.submit(task_2)
 
-            assert task_1.output[0].get(None) == 0
-            assert task_2.output[0].get(None) == 0
+    assert task_1.output[0].get(None) == 0
+    assert task_2.output[0].get(None) == 0
 
-        sleep(1)
-        assert task_1.output[0].get(None) == 0
-        assert task_2.output[0].get(None) == 42
+    sleep_mult_2.unblock()
+    assert task_1.output[0].get(None) == 0
+    assert task_2.output[0].get(None) == 42
 
-    sleep(1)
+    sleep_mult_1.unblock()
     assert task_1.output[0].get(None) == 42
     assert task_2.output[0].get(None) == 42
 
 
-def _create_task(function, nb_outputs=1):
+def _create_task(function):
     task_name = str(uuid.uuid4())
+    output_name = str(uuid.uuid4())
     input_ds = [
         EmbeddedDataSourceEntity.create("input1", Scope.PIPELINE, data=21),
         EmbeddedDataSourceEntity.create("input2", Scope.PIPELINE, data=2),
     ]
-    output_ds = [
-        EmbeddedDataSourceEntity.create(str(uuid.uuid4()), Scope.PIPELINE, data=0)
-        for _ in range(nb_outputs)
-    ]
+    output_ds = [EmbeddedDataSourceEntity.create(output_name, Scope.PIPELINE, data=0)]
     return TaskEntity(
         task_name,
         input=input_ds,
