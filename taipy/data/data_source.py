bb613fa36f2618a69fc95102e309931d1e5a1e45
Jean-Robin <jeanrobin.medori@avaiga.com>
2021-10-22 17:45:57 +0200

    Update taipy/data/data_source.py
    Co-authored-by: Vuillemot Florian <florian.vuillemot@posteo.net>

--- taipy/data/data_source.py
+++ taipy/data/data_source.py
@@ -35,7 +35,7 @@ class DataSource:
     def __eq__(self, other):
         return self.id == other.id
     def __ne__(self, other):
-        return not self.__eq__(other)
+        return not self == other
 
     def __hash__(self):
         return hash(self.id)
