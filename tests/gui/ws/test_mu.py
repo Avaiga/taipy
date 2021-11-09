fbb0e1c7520db9a6dc769aaea89fff68ac0f00c4
dinhlongnguyen <dinhlongviolin1@gmail.com>
2021-11-10 02:27:57 +0700

    add mu test

--- tests/gui/ws/test_mu.py
+++ tests/gui/ws/test_mu.py
@@ -1,20 +1,20 @@
 from taipy.gui import Gui, Markdown
 
 
-def test_ws_number(gui: Gui):
-    # Bind a variable
-    assert gui.bind_var_val("var", 10)
+def mu_template_assert(gui, value_before_update, value_after_update, payload):
+    # Bind test variable
+    assert gui.bind_var_val("var", value_before_update)
     # Bind a page so that the variable will be evaluated as expression
     gui.add_page("test", Markdown("<|{var}|>"))
     gui.run(run_server=False)
-    assert gui.var == 10
+    assert gui.var == value_before_update
     flask_client = gui._server.test_client()
     # Get the jsx once so that the page will be evaluated -> variable will be registered
     flask_client.get("/flask-jsx/test/")
     # WS client and emit
     ws_client = gui._server._ws.test_client(gui._server)
-    ws_client.emit("message", {"type": "U", "name": "var", "payload": "11"})
-    assert gui.var == 11
+    ws_client.emit("message", {"type": "U", "name": "var", "payload": payload})
+    assert gui.var == value_after_update
     # assert for received message (message that would be sent to the frontend client)
     received_message = ws_client.get_received()[0]
     assert isinstance(received_message, dict)
@@ -25,4 +25,18 @@ def test_ws_number(gui: Gui):
     assert "payload" in args
     payload = args["payload"][0]
     assert "name" in payload and payload["name"] == "var"
-    assert "payload" in payload and payload["payload"]["value"] == 11
+    assert "payload" in payload and payload["payload"]["value"] == value_after_update
+
+
+def test_ws_mu_string(gui: Gui):
+    value_before_update = "a random string"
+    value_after_update = "a random string is added"
+    payload = value_after_update
+    mu_template_assert(gui, value_before_update, value_after_update, payload)
+    
+def test_ws_mu_number(gui: Gui):
+    value_before_update = 10
+    value_after_update = 11
+    payload = str(value_after_update)
+    mu_template_assert(gui, value_before_update, value_after_update, payload)
+
