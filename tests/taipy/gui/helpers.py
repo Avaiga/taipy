88f1b0034a34ba0f202c088e6a44b4090468642b
dinhlongnguyen <dinhlongviolin1@gmail.com>
2021-11-12 17:44:30 +0700

    cleanup helpers.py

--- tests/taipy/gui/helpers.py
+++ tests/taipy/gui/helpers.py
@@ -15,24 +15,15 @@ class Helpers:
     @staticmethod
     def test_control_md(gui: Gui, md_string: str, expected_values: t.Union[str, t.List]):
         gui.add_page("test", Markdown(md_string))
-        gui.run(run_server=False)
-        client = gui._server.test_client()
-        response = client.get("/flask-jsx/test/")
-        response_data = json.loads(response.get_data().decode("utf-8", "ignore"))
-        assert response.status_code == 200
-        assert isinstance(response_data, object)
-        assert "jsx" in response_data
-        jsx = response_data["jsx"]
-        logging.getLogger().debug(jsx)
-        if isinstance(expected_values, str):
-            assert jsx == expected_values
-        elif isinstance(expected_values, list):
-            for expected_value in expected_values:
-                assert expected_value in jsx
+        Helpers._test_control(gui, expected_values)
 
     @staticmethod
     def test_control_html(gui: Gui, html_string: str, expected_values: t.Union[str, t.List]):
         gui.add_page("test", Html(html_string))
+        Helpers._test_control(gui, expected_values)
+
+    @staticmethod
+    def _test_control(gui: Gui, expected_values: t.Union[str, t.List]):
         gui.run(run_server=False)
         client = gui._server.test_client()
         response = client.get("/flask-jsx/test/")
