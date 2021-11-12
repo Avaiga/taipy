a71f55e524daf86ad9fe97e65a26d895216a109b
Fred Lefevere-Laoide <Fred.Lefevere-Laoide@Avaiga.com>
2021-11-12 12:40:34 +0100

    Move Date data from localStorage to Taipy Context  #304 - local storage   - timezone   - theme - state  - date/time formatting (with TimeZone)  - umber formatting

--- gui/src/components/Taipy/DateSelector.tsx
+++ gui/src/components/Taipy/DateSelector.tsx
@@ -8,7 +8,7 @@ import { TaipyContext } from "../../context/taipyContext";
 import { createSendUpdateAction } from "../../context/taipyReducers";
 import { TaipyBaseProps } from "./utils";
 import { getDateTime, getClientServerTimeZoneOffset } from "../../utils";
-import { useDynamicProperty } from "../../utils/hooks";
+import { useDynamicProperty, useFormatConfig } from "../../utils/hooks";
 
 interface DateSelectorProps extends TaipyBaseProps {
     withTime?: boolean;
@@ -18,9 +18,10 @@ interface DateSelectorProps extends TaipyBaseProps {
 
 const DateSelector = (props: DateSelectorProps) => {
     const { className, tp_varname, withTime = false, id, propagate = true } = props;
-
-    const [value, setValue] = useState(() => getDateTime(props.defaultValue));
     const { dispatch } = useContext(TaipyContext);
+    const formatConfig = useFormatConfig();
+    const tz = formatConfig.timeZone;
+    const [value, setValue] = useState(() => getDateTime(props.defaultValue, tz));
 
     const active = useDynamicProperty(props.active, props.defaultActive, true);
 
@@ -30,8 +31,8 @@ const DateSelector = (props: DateSelectorProps) => {
             const newDate = new Date(v);
             if (isValid(newDate)) {
                 // dispatch new date which offset by the timeZone differences between client and server
-                const hours = getClientServerTimeZoneOffset() / 60;
-                const minutes = getClientServerTimeZoneOffset() % 60;
+                const hours = getClientServerTimeZoneOffset(tz) / 60;
+                const minutes = getClientServerTimeZoneOffset(tz) % 60;
                 newDate.setSeconds(0);
                 newDate.setMilliseconds(0);
                 if (withTime) {
@@ -46,17 +47,20 @@ const DateSelector = (props: DateSelectorProps) => {
                 dispatch(createSendUpdateAction(tp_varname, newDate.toISOString(), propagate));
             }
         },
-        [tp_varname, dispatch, withTime, propagate]
+        [tp_varname, dispatch, withTime, propagate, tz]
     );
 
-    const renderInput = useCallback((params) => <TextField id={id} {...params} className={className} />, [id, className]);
+    const renderInput = useCallback(
+        (params) => <TextField id={id} {...params} className={className} />,
+        [id, className]
+    );
 
     // Run every time props.value get updated
     useEffect(() => {
         if (props.value !== undefined) {
-            setValue(getDateTime(props.value));
+            setValue(getDateTime(props.value, tz));
         }
-    }, [props.value]);
+    }, [props.value, tz]);
 
     return withTime ? (
         <DateTimePicker
