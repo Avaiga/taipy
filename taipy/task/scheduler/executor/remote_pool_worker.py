5634fd5e7315a4b78bc084ad073ece23596ae3e9
Vuillemot Florian <florian.vuillemot@posteo.net>
2021-11-18 15:24:41 +0100

    Specify broker hostname for remote worker from config (#398)

--- taipy/task/scheduler/executor/remote_pool_worker.py
+++ taipy/task/scheduler/executor/remote_pool_worker.py
@@ -1,3 +1,5 @@
+from taipy.config import Config
 from taipy.task.scheduler.executor.remote_pool_executor import RemotePoolExecutor
 
-RemotePoolExecutor(None).as_worker()
+task_scheduler_config = Config.task_scheduler_configs.create()
+RemotePoolExecutor(None, task_scheduler_config.hostname).as_worker()
