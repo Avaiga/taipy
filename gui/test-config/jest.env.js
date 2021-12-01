8ca5d260e14dceabddf36ca3828c399a9a0fdd18
dinhlongnguyen <dinhlongviolin1@gmail.com>
2021-12-01 15:01:58 +0700

    fix jest test for apache-arrow

--- gui/test-config/jest.env.js
+++ gui/test-config/jest.env.js
@@ -1,3 +1,6 @@
 // setup file for jest
 const dotenv = require('dotenv');
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 dotenv.config({ path: './.env.test' });
