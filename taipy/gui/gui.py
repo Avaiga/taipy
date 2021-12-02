2b4326c904ab8a4232622984792701e20031e562
dinhlongnguyen <dinhlongviolin1@gmail.com>
2021-12-02 20:20:18 +0700

    wording agagin

--- taipy/gui/gui.py
+++ taipy/gui/gui.py
@@ -720,7 +720,7 @@ class Gui(object, metaclass=Singleton):
                         app_config[key] = value if app_config[key] is None else type(app_config[key])(value)  # type: ignore
                     except ValueError as ve:
                         warnings.warn(
-                            f"Invalid env value in Gui.run {key} - {value}. Unable to parse to the correct type.\n{ve}"
+                            f"Invalid env value in Gui.run {key} - {value}. Unable to parse value to the correct type.\n{ve}"
                         )
         # Load keyword arguments
         for key, value in kwargs.items():
@@ -730,7 +730,7 @@ class Gui(object, metaclass=Singleton):
                     app_config[key] = value if app_config[key] is None else type(app_config[key])(value)  # type: ignore
                 except ValueError as ve:
                     warnings.warn(
-                        f"Invalid keyword arguments value in Gui.run {key} - {value}. Unable to parse to the correct type.\n{ve}"
+                        f"Invalid keyword arguments value in Gui.run {key} - {value}. Unable to parse value to the correct type.\n{ve}"
                     )
         # Register taipy.gui markdown extensions for Markdown renderer
         Gui._markdown.registerExtensions(extensions=["taipy.gui"], configs={})
