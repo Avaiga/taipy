f8d98711d51c0c39ba1c5db23a6fc0eca837f39d
João André <joao.dasilva@avaiga.com>
2021-12-14 20:31:12 -0300

    Rewrite connection string

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/avaiga/taipy",
-    version="0.1.1",
+    version="0.1.2",
     zip_safe=False,
 )
--- taipy/data/sql.py
+++ taipy/data/sql.py
@@ -1,6 +1,8 @@
+import urllib
 from datetime import datetime
 from typing import Any, Dict, List, Optional, Tuple
 
+import numpy as np
 import pandas as pd
 from sqlalchemy import MetaData, create_engine, table, text
 
@@ -63,8 +65,10 @@ class SQLDataSource(DataSource):
         self.__engine = self.__create_engine(
             properties.get("db_engine"),
             properties.get("db_username"),
+            properties.get("db_host", "localhost"),
             properties.get("db_password"),
             properties.get("db_name"),
+            properties.get("db_port", 1433),
         )
 
         super().__init__(
@@ -85,14 +89,17 @@ class SQLDataSource(DataSource):
             self.unlock_edition()
 
     @staticmethod
-    def __build_conn_string(engine, username, password, database):
+    def __build_conn_string(engine, username, host, password, database, port):
         # TODO: Add support to other SQL engines
         if engine == "mssql":
-            return f"{engine}+pyodbc://{username}:{password}@{database}"
+            return "mssql+pyodbc:///?odbc_connect=" + urllib.parse.quote_plus(
+                f"DRIVER=FreeTDS;SERVER={host};PORT={port};DATABASE={database};UID={username};PWD={password};TDS_Version=8.0;"
+            )
         raise UnknownDatabaseEngine(f"Unknown engine: {engine}")
 
-    def __create_engine(self, engine, username, password, database):
-        return create_engine(self.__build_conn_string(engine, username, password, database))
+    def __create_engine(self, engine, username, host, password, database, port=1433):
+        conn_str = self.__build_conn_string(engine, username, host, password, database, port)
+        return create_engine(conn_str)
 
     @classmethod
     def storage_type(cls) -> str:
@@ -125,15 +132,42 @@ class SQLDataSource(DataSource):
                 self.__insert_dicts([data], write_table, connection)
             elif isinstance(data, tuple):
                 self.__insert_tuples([data], write_table, connection)
-            elif isinstance(data, list):
+            elif isinstance(data, list) or isinstance(data, np.ndarray):
                 if isinstance(data[0], tuple):
                     self.__insert_tuples(data, write_table, connection)
                 elif isinstance(data[0], dict):
                     self.__insert_dicts(data, write_table, connection)
+                else:
+                    self.__insert_list(data, write_table, connection)
+                    return
             else:
                 # if data is a single primitive value (int, str, etc), pass it as a list of tuples
                 # with only one value
                 self.__insert_tuples([(data,)], write_table, connection)
+
+    @staticmethod
+    def __insert_list(data: List[Tuple], write_table: Any, connection: Any) -> None:
+        """
+        :param data: a list of values
+        :param write_table: a SQLAlchemy object that represents a table
+        :param connection: a SQLAlchemy connection to write the data
+
+        This method will lookup the length of the first object of the list and build the insert through
+        creation of a string of '?' equivalent to the length of the element. The '?' character is used as
+        placeholder for a tuple of same size.
+        """
+        with connection.begin() as transaction:
+            try:
+                markers = ",".join(["(?)"] * len(data))
+                markers = markers
+                ins = "INSERT INTO {tablename} VALUES {markers}"
+                ins = ins.format(tablename=write_table.name, markers=markers)
+                connection.execute(ins, list(data))
+            except:
+                transaction.rollback()
+                raise
+            else:
+                transaction.commit()
 
     @staticmethod
     def __insert_tuples(data: List[Tuple], write_table: Any, connection: Any) -> None:
@@ -152,6 +186,7 @@ class SQLDataSource(DataSource):
                 markers = markers
                 ins = "INSERT INTO {tablename} VALUES ({markers})"
                 ins = ins.format(tablename=write_table.name, markers=markers)
+
                 connection.execute(ins, data)
             except:
                 transaction.rollback()
