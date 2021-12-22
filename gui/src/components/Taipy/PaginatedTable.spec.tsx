bd700c849c2b8cfd3fad03f754557cd625f26c46
Fred Lefevere-Laoide <Fred.Lefevere-Laoide@Avaiga.com>
2021-12-22 18:35:59 +0100

    fix tests

--- gui/src/components/Taipy/PaginatedTable.spec.tsx
+++ gui/src/components/Taipy/PaginatedTable.spec.tsx
@@ -10,143 +10,216 @@ import { TableValueType } from "./tableUtils";
 
 const tableValue = {
     "0-100--asc": {
-        "data": [
+        data: [
             {
-                "Day_str": "2020-04-01T00:00:00.000000Z",
+                Day_str: "2020-04-01T00:00:00.000000Z",
                 "Daily hospital occupancy": 856,
-                "Entity": "Austria",
-                "Code": "AUT"
+                Entity: "Austria",
+                Code: "AUT",
             },
             {
-                "Day_str": "2020-04-02T00:00:00.000000Z",
+                Day_str: "2020-04-02T00:00:00.000000Z",
                 "Daily hospital occupancy": 823,
-                "Entity": "Austria",
-                "Code": "AUT"
+                Entity: "Austria",
+                Code: "AUT",
             },
             {
-                "Day_str": "2020-04-03T00:00:00.000000Z",
+                Day_str: "2020-04-03T00:00:00.000000Z",
                 "Daily hospital occupancy": 829,
-                "Entity": "Austria",
-                "Code": "AUT"
+                Entity: "Austria",
+                Code: "AUT",
             },
             {
-                "Day_str": "2020-04-04T00:00:00.000000Z",
+                Day_str: "2020-04-04T00:00:00.000000Z",
                 "Daily hospital occupancy": 826,
-                "Entity": "Austria",
-                "Code": "AUT"
+                Entity: "Austria",
+                Code: "AUT",
             },
             {
-                "Day_str": "2020-04-05T00:00:00.000000Z",
+                Day_str: "2020-04-05T00:00:00.000000Z",
                 "Daily hospital occupancy": 712,
-                "Entity": "Austria",
-                "Code": "AUT"
+                Entity: "Austria",
+                Code: "AUT",
             },
             {
-                "Day_str": "2020-04-06T00:00:00.000000Z",
+                Day_str: "2020-04-06T00:00:00.000000Z",
                 "Daily hospital occupancy": 824,
-                "Entity": "Austria",
-                "Code": "AUT"
+                Entity: "Austria",
+                Code: "AUT",
             },
             {
-                "Day_str": "2020-04-07T00:00:00.000000Z",
+                Day_str: "2020-04-07T00:00:00.000000Z",
                 "Daily hospital occupancy": 857,
-                "Entity": "Austria",
-                "Code": "AUT"
+                Entity: "Austria",
+                Code: "AUT",
             },
             {
-                "Day_str": "2020-04-08T00:00:00.000000Z",
+                Day_str: "2020-04-08T00:00:00.000000Z",
                 "Daily hospital occupancy": 829,
-                "Entity": "Austria",
-                "Code": "AUT"
+                Entity: "Austria",
+                Code: "AUT",
             },
             {
-                "Day_str": "2020-04-09T00:00:00.000000Z",
+                Day_str: "2020-04-09T00:00:00.000000Z",
                 "Daily hospital occupancy": 820,
-                "Entity": "Austria",
-                "Code": "AUT"
+                Entity: "Austria",
+                Code: "AUT",
             },
             {
-                "Day_str": "2020-04-10T00:00:00.000000Z",
+                Day_str: "2020-04-10T00:00:00.000000Z",
                 "Daily hospital occupancy": 771,
-                "Entity": "Austria",
-                "Code": "AUT"
-            }
+                Entity: "Austria",
+                Code: "AUT",
+            },
         ],
-        "rowcount": 14477,
-        "start": 0
-    }
+        rowcount: 14477,
+        start: 0,
+    },
 };
-const tableColumns = JSON.stringify({"Entity": {dfid: "Entity"}, "Daily hospital occupancy": {dfid: "Daily hospital occupancy", type:"int64"}});
+const tableColumns = JSON.stringify({
+    Entity: { dfid: "Entity" },
+    "Daily hospital occupancy": { dfid: "Daily hospital occupancy", type: "int64" },
+});
 
 describe("PaginatedTable Component", () => {
     it("renders", async () => {
-        const { getByText } = render(<PaginatedTable value={undefined} columns={tableColumns}  />);
+        const { getByText } = render(<PaginatedTable value={undefined} columns={tableColumns} />);
         const elt = getByText("Entity");
-        expect(elt.tagName).toBe("SPAN");
+        expect(elt.tagName).toBe("DIV");
     });
     it("displays the right info for class", async () => {
-        const { getByText } = render(<PaginatedTable value={undefined} columns={tableColumns} className="taipy-table" />);
+        const { getByText } = render(
+            <PaginatedTable value={undefined} columns={tableColumns} className="taipy-table" />
+        );
         const elt = getByText("Entity").closest("table");
         expect(elt).toHaveClass("taipy-table");
     });
     it("is disabled", async () => {
         const { getByText } = render(<PaginatedTable value={undefined} columns={tableColumns} active={false} />);
         const elt = getByText("Entity");
-        expect(elt).toHaveClass("Mui-disabled");
+        expect(elt.parentElement).toHaveClass("Mui-disabled");
     });
     it("is enabled by default", async () => {
         const { getByText } = render(<PaginatedTable value={undefined} columns={tableColumns} />);
         const elt = getByText("Entity");
-        expect(elt).not.toHaveClass("Mui-disabled");
+        expect(elt.parentElement).not.toHaveClass("Mui-disabled");
     });
     it("is enabled by active", async () => {
         const { getByText } = render(<PaginatedTable value={undefined} columns={tableColumns} active={true} />);
         const elt = getByText("Entity");
-        expect(elt).not.toHaveClass("Mui-disabled");
+        expect(elt.parentElement).not.toHaveClass("Mui-disabled");
     });
     it("dispatch 2 well formed messages at first render", async () => {
         const dispatch = jest.fn();
         const state: TaipyState = INITIAL_STATE;
-        render(<TaipyContext.Provider value={{ state, dispatch }}>
+        render(
+            <TaipyContext.Provider value={{ state, dispatch }}>
                 <PaginatedTable id="table" value={undefined} columns={tableColumns} tp_updatevars="varname=varname" />
-            </TaipyContext.Provider>);
-        expect(dispatch).toHaveBeenCalledWith({"name": "", "payload": {"id": "table", "names": ["varname"]}, "type": "REQUEST_UPDATE"});
-        expect(dispatch).toHaveBeenCalledWith({"name": "", "payload": {"columns": ["Entity", "Daily hospital occupancy"],"end": 100,"id": "table","orderby": "","pagekey": "0-100--asc","sort": "asc","start": 0 },"type": "REQUEST_DATA_UPDATE"});
+            </TaipyContext.Provider>
+        );
+        expect(dispatch).toHaveBeenCalledWith({
+            name: "",
+            payload: { id: "table", names: ["varname"] },
+            type: "REQUEST_UPDATE",
+        });
+        expect(dispatch).toHaveBeenCalledWith({
+            name: "",
+            payload: {
+                columns: ["Entity", "Daily hospital occupancy"],
+                end: 100,
+                id: "table",
+                orderby: "",
+                pagekey: "0-100--asc",
+                sort: "asc",
+                start: 0,
+                aggregates: [],
+                applies: undefined,
+            },
+            type: "REQUEST_DATA_UPDATE",
+        });
     });
     it("dispatch a well formed message on sort", async () => {
         const dispatch = jest.fn();
         const state: TaipyState = INITIAL_STATE;
-        const { getByText } = render(<TaipyContext.Provider value={{ state, dispatch }}>
-            <PaginatedTable value={undefined} columns={tableColumns} />
-            </TaipyContext.Provider>);
+        const { getByText } = render(
+            <TaipyContext.Provider value={{ state, dispatch }}>
+                <PaginatedTable value={undefined} columns={tableColumns} />
+            </TaipyContext.Provider>
+        );
         const elt = getByText("Entity");
         userEvent.click(elt);
-        expect(dispatch).toHaveBeenCalledWith({"name": "", "payload": {"columns": ["Entity", "Daily hospital occupancy"],"end": 100,"id": undefined,"orderby": "Entity","pagekey": "0-100-Entity-asc","sort": "asc","start": 0,}, "type": "REQUEST_DATA_UPDATE"});
+        expect(dispatch).toHaveBeenCalledWith({
+            name: "",
+            payload: {
+                columns: ["Entity", "Daily hospital occupancy"],
+                end: 100,
+                id: undefined,
+                orderby: "Entity",
+                pagekey: "0-100-Entity-asc",
+                sort: "asc",
+                start: 0,
+                aggregates: [],
+                applies: undefined,
+            },
+            type: "REQUEST_DATA_UPDATE",
+        });
     });
     it("dispatch a well formed message on page change", async () => {
         const dispatch = jest.fn();
-        const state: TaipyState = {...INITIAL_STATE, data: {table: undefined} };
-        const { getByLabelText, rerender } = render(<TaipyContext.Provider value={{ state, dispatch }}>
-                <PaginatedTable id="table" value={state.data.table as undefined} columns={tableColumns} tp_updatevars="varname=varname" />
-            </TaipyContext.Provider>);
-        const newState = {...state, data: {...state.data, table: tableValue}}
-        rerender(<TaipyContext.Provider value={{ state: newState, dispatch }}>
-            <PaginatedTable id="table" value={newState.data.table as TableValueType} columns={tableColumns} tp_updatevars="varname=varname" />
-        </TaipyContext.Provider>);
+        const state: TaipyState = { ...INITIAL_STATE, data: { table: undefined } };
+        const { getByLabelText, rerender } = render(
+            <TaipyContext.Provider value={{ state, dispatch }}>
+                <PaginatedTable
+                    id="table"
+                    value={state.data.table as undefined}
+                    columns={tableColumns}
+                    tp_updatevars="varname=varname"
+                />
+            </TaipyContext.Provider>
+        );
+        const newState = { ...state, data: { ...state.data, table: tableValue } };
+        rerender(
+            <TaipyContext.Provider value={{ state: newState, dispatch }}>
+                <PaginatedTable
+                    id="table"
+                    value={newState.data.table as TableValueType}
+                    columns={tableColumns}
+                    tp_updatevars="varname=varname"
+                />
+            </TaipyContext.Provider>
+        );
         const elt = getByLabelText("Go to next page");
         userEvent.click(elt);
-        expect(dispatch).toHaveBeenCalledWith({"name": "", "payload": {"columns": ["Entity", "Daily hospital occupancy"],"end": 200,"id": "table","orderby": "","pagekey": "100-200--asc","sort": "asc","start": 100}, "type": "REQUEST_DATA_UPDATE"});
+        expect(dispatch).toHaveBeenCalledWith({
+            name: "",
+            payload: {
+                columns: ["Entity", "Daily hospital occupancy"],
+                end: 200,
+                id: "table",
+                orderby: "",
+                pagekey: "100-200--asc",
+                sort: "asc",
+                start: 100,
+                aggregates: [],
+                applies: undefined,
+            },
+            type: "REQUEST_DATA_UPDATE",
+        });
     });
     it("displays the received data", async () => {
         const dispatch = jest.fn();
         const state: TaipyState = INITIAL_STATE;
-        const { getAllByText, rerender } = render(<TaipyContext.Provider value={{ state, dispatch }}>
+        const { getAllByText, rerender } = render(
+            <TaipyContext.Provider value={{ state, dispatch }}>
                 <PaginatedTable value={undefined} columns={tableColumns} />
-            </TaipyContext.Provider>);
+            </TaipyContext.Provider>
+        );
 
-        rerender(<TaipyContext.Provider value={{ state, dispatch }}>
-            <PaginatedTable value={tableValue as TableValueType} columns={tableColumns} />
-        </TaipyContext.Provider>);
+        rerender(
+            <TaipyContext.Provider value={{ state, dispatch }}>
+                <PaginatedTable value={tableValue as TableValueType} columns={tableColumns} />
+            </TaipyContext.Provider>
+        );
         const elts = getAllByText("Austria");
         expect(elts.length).toBeGreaterThan(1);
         expect(elts[0].tagName).toBe("TD");
@@ -162,11 +235,7 @@ describe("PaginatedTable Component", () => {
         );
         rerender(
             <TaipyContext.Provider value={{ state, dispatch }}>
-                <PaginatedTable
-                    selected={selected}
-                    value={tableValue as TableValueType}
-                    columns={tableColumns}
-                />
+                <PaginatedTable selected={selected} value={tableValue as TableValueType} columns={tableColumns} />
             </TaipyContext.Provider>
         );
         const elts = getAllByText("Austria");
