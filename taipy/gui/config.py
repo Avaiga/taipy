b58db411441114f9cc59d0d52891d02b6acdf020
dinhlongnguyen <dinhlongviolin1@gmail.com>
2021-12-23 10:40:03 +0700

    use_reloader, option in command line

--- taipy/gui/config.py
+++ taipy/gui/config.py
@@ -1,8 +1,12 @@
+import os
+import re
+import sys
 import typing as t
 import warnings
 
 import pytz
 import tzlocal
+from dotenv import dotenv_values
 
 from .page import Page, Partial
 
@@ -11,6 +15,7 @@ AppConfigOption = t.Literal[
     "dark_mode",
     "debug",
     "host",
+    "use_reloader",
     "time_zone",
     "propagate",
     "client_url",
@@ -32,6 +37,7 @@ AppConfig = t.TypedDict(
         "dark_mode": bool,
         "debug": bool,
         "host": str,
+        "use_reloader": bool,
         "time_zone": str,
         "propagate": bool,
         "client_url": str,
@@ -74,6 +80,11 @@ StyleConfig = t.TypedDict(
 
 
 class GuiConfig(object):
+
+    __RE_PORT_NUMBER = re.compile(
+        r"^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$"
+    )
+
     def __init__(self):
         self.pages: t.List[Page] = []
         self.routes: t.List[str] = []
@@ -114,3 +125,65 @@ class GuiConfig(object):
             )
         # return user defined IANA Time Zone (https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
         return self.app_config["time_zone"]
+
+    def load_app_config_runtime(self, root_dir, env_filename, kwargs):  # noqa: C901
+        app_config = self.app_config
+        env_file_abs_path = env_filename if os.path.isabs(env_filename) else os.path.join(root_dir, env_filename)
+        if os.path.isfile(env_file_abs_path):
+            for key, value in dotenv_values(env_file_abs_path).items():
+                key = key.lower()
+                if value is not None and key in app_config:
+                    try:
+                        app_config[key] = value if app_config[key] is None else type(app_config[key])(value)  # type: ignore
+                    except Exception as e:
+                        warnings.warn(
+                            f"Invalid env value in Gui.run {key} - {value}. Unable to parse value to the correct type.\n{e}"
+                        )
+        # Load keyword arguments
+        for key, value in kwargs.items():
+            key = key.lower()
+            if value is not None and key in app_config:
+                try:
+                    app_config[key] = value if app_config[key] is None else type(app_config[key])(value)  # type: ignore
+                except Exception as e:
+                    warnings.warn(
+                        f"Invalid keyword arguments value in Gui.run {key} - {value}. Unable to parse value to the correct type.\n{e}"
+                    )
+        # Load from system arguments
+        pointer = 1
+        while pointer < (argv_len := len(argv := sys.argv)):
+            arg = argv[pointer]
+            if arg in ["-p", "--port"]:
+                if pointer == argv_len - 1:
+                    warnings.warn("Missing port value for --port option")
+                    break
+                arg_value = argv[pointer + 1]
+                if not GuiConfig.__RE_PORT_NUMBER.match(arg_value):
+                    warnings.warn("Port value for --port option is not valid")
+                    pointer += 1
+                    continue
+                app_config["port"] = int(arg_value)
+                pointer += 1
+            if arg in ["-h", "--host"]:
+                if pointer == argv_len - 1 or "-" in argv[pointer + 1]:
+                    warnings.warn("Missing host value for --host option")
+                    pointer += 1
+                    continue
+                app_config["host"] = argv[pointer + 1]
+                pointer += 1
+            if arg in ["-c", "--client-url"]:
+                if pointer == argv_len - 1 or "-" in argv[pointer + 1]:
+                    warnings.warn("Missing url value for --client-url option")
+                    pointer += 1
+                    continue
+                app_config["client_url"] = argv[pointer + 1]
+                pointer += 1
+            if arg in ["--debug"]:
+                app_config["debug"] = True
+            if arg in ["--no-debug"]:
+                app_config["debug"] = False
+            if arg in ["--use-reloader"]:
+                app_config["use_reloader"] = True
+            if arg in ["--no-reloader"]:
+                app_config["use_reloader"] = False
+            pointer += 1
--- taipy/gui/server.py
+++ taipy/gui/server.py
@@ -188,5 +188,5 @@ class Server:
     def test_client(self):
         return self._flask.test_client()
 
-    def runWithWS(self, host=None, port=None, debug=None):
-        self._ws.run(self._flask, host=host, port=port, debug=debug)
+    def runWithWS(self, host=None, port=None, debug=None, use_reloader=None):
+        self._ws.run(self._flask, host=host, port=port, debug=debug, use_reloader=use_reloader)
