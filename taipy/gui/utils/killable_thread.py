c9673ae97a4b0003ce17669ffce6a569e2df40be
dinhlongnguyen <dinhlongviolin1@gmail.com>
2022-01-11 14:22:13 +0700

    clean up comments

--- taipy/gui/utils/killable_thread.py
+++ taipy/gui/utils/killable_thread.py
@@ -1,25 +1,18 @@
 import sys
 import threading
-import trace
 
 
 class KillableThread(threading.Thread):
-    """A subclass of threading.Thread, with a kill()
-    method."""
-
     def __init__(self, *args, **keywords):
         threading.Thread.__init__(self, *args, **keywords)
         self.killed = False
 
     def start(self):
-        """Start the thread."""
         self.__run_backup = self.run
-        self.run = self.__run  # Force the Thread toinstall our trace.
+        self.run = self.__run
         threading.Thread.start(self)
 
     def __run(self):
-        """Hacked run function, which installs the
-        trace."""
         sys.settrace(self.globaltrace)
         self.__run_backup()
         self.run = self.__run_backup
@@ -31,9 +24,8 @@ class KillableThread(threading.Thread):
             return None
 
     def localtrace(self, frame, why, arg):
-        if self.killed:
-            if why == "line":
-                raise SystemExit()
+        if self.killed and why == "line":
+            raise SystemExit()
         return self.localtrace
 
     def kill(self):
