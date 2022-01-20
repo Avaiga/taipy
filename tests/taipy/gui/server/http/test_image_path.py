1edaf2f56a25066f713d1dcb71dfca855d798641
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Avaiga.com>
2022-01-20 14:27:01 +0100

    pycodestyle on test file !

--- tests/taipy/gui/server/http/test_image_path.py
+++ tests/taipy/gui/server/http/test_image_path.py
@@ -2,6 +2,7 @@ import pytest
 from taipy.gui import Gui
 import pathlib
 
+
 def test_image_path_not_found(gui: Gui, helpers):
     gui.run(run_server=False)
     flask_client = gui._server.test_client()
@@ -10,8 +11,11 @@ def test_image_path_not_found(gui: Gui, helpers):
     ret = flask_client.get(f"/taipy-images/images/img.png?client_id={sid}")
     assert ret.status_code == 404
 
+
 def test_image_path_found(gui: Gui, helpers):
-    url = gui._get_image_content("img", str((pathlib.Path(__file__).parent.parent.parent / "resources" / "fred.png").resolve()), False)
+    url = gui._get_image_content(
+        "img", str((pathlib.Path(__file__).parent.parent.parent / "resources" / "fred.png").resolve()), False
+    )
     gui.run(run_server=False)
     flask_client = gui._server.test_client()
     # Get the jsx once so that the page will be evaluated -> variable will be registered
