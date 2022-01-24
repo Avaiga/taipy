c92d1d12e97b3a4899681b8c87ad095100906b5f
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Avaiga.com>
2022-01-24 13:43:09 +0100

    pycodestyle

--- tests/taipy/gui/utils/test_filename.py
+++ tests/taipy/gui/utils/test_filename.py
@@ -6,7 +6,8 @@ import pathlib
 
 
 def test_empty_file_name(gui: Gui, helpers):
-    assert _get_non_existent_file_path(pathlib.Path(tempfile.gettempdir()), '').name 
+    assert _get_non_existent_file_path(pathlib.Path(tempfile.gettempdir()), "").name
+
 
 def test_non_existent_file(gui: Gui, helpers):
-    assert not _get_non_existent_file_path(pathlib.Path(tempfile.gettempdir()), '').exists()
+    assert not _get_non_existent_file_path(pathlib.Path(tempfile.gettempdir()), "").exists()
