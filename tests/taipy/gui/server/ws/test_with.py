20a618a8210212d1f238b30dca3419b6ae700dd0
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Avaiga.com>
2022-01-24 14:23:16 +0100

    test message grouping

--- tests/taipy/gui/server/ws/test_with.py
+++ tests/taipy/gui/server/ws/test_with.py
@@ -1,12 +1,27 @@
-from taipy.gui import Gui
+from taipy.gui import Gui, Markdown
 
 
 def test_sending_messages_in_group(gui: Gui, helpers):
-    gui.bind_var_val("name", "World!")
-    gui.bind_var_val("btn_id", "button1")
-    md_string = "<|Hello {name}|button|id={btn_id}|>"
-    with gui as aGui:
-        setattr(aGui, "name", "Monde!")
-        setattr(aGui, "btn_id", "button2")
-    expected_list = ["<Button", 'defaultLabel="Hello Monde!"', 'id="button1"', "label={tp_Hello_name"]
-    helpers.test_control_md(gui, md_string, expected_list)
+    name = "World!"
+    btn_id = "button1"
+    gui.add_page("test", Markdown("<|Hello {name}|button|id={btn_id}|>"))
+    gui.run(run_server=False)
+    flask_client = gui._server.test_client()
+    # WS client and emit
+    ws_client = gui._server._ws.test_client(gui._server.get_flask())
+    sid = helpers.create_scope_and_get_sid(gui)
+    # Get the jsx once so that the page will be evaluated -> variable will be registered
+    flask_client.get(f"/taipy-jsx/test/?client_id={sid}")
+    assert gui._scopes.get_all_scopes()[sid].name == "World!"  # type: ignore
+    assert gui._scopes.get_all_scopes()[sid].btn_id == "button1"  # type: ignore
+
+    with gui.get_flask_app().test_request_context(f"/taipy-jsx/test/?client_id={sid}", data={"client_id": "global"}):
+        with gui as aGui:
+            setattr(aGui, "name", "Monde!")
+            setattr(aGui, "btn_id", "button2")
+
+    assert gui._scopes.get_all_scopes()[sid].name == "Monde!"
+    assert gui._scopes.get_all_scopes()[sid].btn_id == "button2"  # type: ignore
+
+    received_messages = ws_client.get_received()
+    helpers.assert_outward_ws_multiple_message(received_messages[0], "MS", 2)
