e464569f09a46232495866610fc725624b39b118
toan-quach <toan.quach@avaiga.com>
2022-01-26 17:59:47 +0700

    added expected example

--- tests/taipy/data/test_filter_data_node.py
+++ tests/taipy/data/test_filter_data_node.py
@@ -93,7 +93,19 @@ class TestFilterDataNode:
         assert isinstance(filtered_custom_ds, FilterDataNode)
         assert isinstance(filtered_custom_ds.data, List)
         assert all(map(lambda x: isinstance(x, CustomClass), filtered_custom_ds.data))
-        print([(i, (d.a, d.b)) for i, d in enumerate(filtered_custom_ds.data)])
+
+        from copy import deepcopy
+
+        expected_filtered_custom_ds = deepcopy(custom_ds._read())
+
+        for col in bool_df.columns:
+            for i, row in bool_df[col].iteritems():
+                setattr(
+                    expected_filtered_custom_ds[i], col, getattr(expected_filtered_custom_ds[i], col) if row else None
+                )
+
+        print(f"Expected: {[(i, (d.a, d.b)) for i, d in enumerate(expected_filtered_custom_ds)]}")
+        print(f"Result: {[(i, (d.a, d.b)) for i, d in enumerate(filtered_custom_ds.data)]}")
         print(bool_df)
 
         bool_1d_index = [True if i < 5 else False for i in range(10)]
