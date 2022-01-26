35252ff59119077a59ce244f7dcf15ef7d787065
Toan Quach <93168955+toan-quach@users.noreply.github.com>
2022-01-26 10:11:26 +0700

    Update taipy/data/excel.py
    Co-authored-by: Jean-Robin <jeanrobin.medori@avaiga.com>

--- taipy/data/excel.py
+++ taipy/data/excel.py
@@ -33,7 +33,7 @@ class ExcelDataSource(DataSource):
         job_ids (List[str]): Ordered list of jobs that have written this data source.
         up_to_date (bool): `True` if the data is considered as up to date. `False` otherwise.
         properties (list): List of additional arguments. Note that the properties parameter should at least contain
-            values for "path" properties.
+           a value for "path" properties.
     """
 
     __STORAGE_TYPE = "excel"
