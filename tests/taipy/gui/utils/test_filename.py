d190efc6e25080a34e3671acc274cd22e93246b1
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Avaiga.com>
2022-01-28 07:24:22 +0100

    formatting

--- tests/taipy/gui/utils/test_filename.py
+++ tests/taipy/gui/utils/test_filename.py
@@ -12,7 +12,8 @@ def test_empty_file_name(gui: Gui, helpers):
 def test_non_existent_file(gui: Gui, helpers):
     assert not _get_non_existent_file_path(pathlib.Path(tempfile.gettempdir()), "").exists()
 
-def test_existent_file(gui:Gui, helpers):
+
+def test_existent_file(gui: Gui, helpers):
     file_path = _get_non_existent_file_path(pathlib.Path(tempfile.gettempdir()), "")
     with open(file_path, "w") as file_handler:
         file_handler.write("hello")
@@ -27,5 +28,3 @@ def test_existent_file(gui:Gui, helpers):
     assert file_path.exists()
     file_path = _get_non_existent_file_path(pathlib.Path(tempfile.gettempdir()), "")
     assert file_path.name == f"{file_stem}.{index + 2}{file_suffix}"
-    
-    
