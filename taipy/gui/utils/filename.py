649e849effea36d216641d1636e149fbfae630de
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Avaiga.com>
2022-01-28 07:23:39 +0100

    #779 non existent file name

--- taipy/gui/utils/filename.py
+++ taipy/gui/utils/filename.py
@@ -7,7 +7,9 @@ def _get_non_existent_file_path(dir_path: Path, file_name: str) -> Path:
         file_name = "taipy_file.bin"
     file_path = dir_path / file_name
     index = 0
+    file_stem = file_path.stem
+    file_suffix = file_path.suffix
     while file_path.exists():
-        file_path = dir_path / f"{file_path.stem}.{index}{file_path.suffix}"
+        file_path = dir_path / f"{file_stem}.{index}{file_suffix}"
         index += 1
     return file_path
--- tests/taipy/gui/utils/test_filename.py
+++ tests/taipy/gui/utils/test_filename.py
@@ -11,3 +11,21 @@ def test_empty_file_name(gui: Gui, helpers):
 
 def test_non_existent_file(gui: Gui, helpers):
     assert not _get_non_existent_file_path(pathlib.Path(tempfile.gettempdir()), "").exists()
+
+def test_existent_file(gui:Gui, helpers):
+    file_path = _get_non_existent_file_path(pathlib.Path(tempfile.gettempdir()), "")
+    with open(file_path, "w") as file_handler:
+        file_handler.write("hello")
+    assert file_path.exists()
+    file_stem = file_path.stem.split(".", 1)[0]
+    file_suffix = file_path.suffixes[-1]
+    index = int(file_path.suffixes[0][1:]) if len(file_path.suffixes) > 1 else -1
+    file_path = _get_non_existent_file_path(pathlib.Path(tempfile.gettempdir()), "")
+    assert file_path.name == f"{file_stem}.{index + 1}{file_suffix}"
+    with open(file_path, "w") as file_handler:
+        file_handler.write("hello 2")
+    assert file_path.exists()
+    file_path = _get_non_existent_file_path(pathlib.Path(tempfile.gettempdir()), "")
+    assert file_path.name == f"{file_stem}.{index + 2}{file_suffix}"
+    
+    
