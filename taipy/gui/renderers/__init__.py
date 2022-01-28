38a0c116fc70165dfa059fdf7f751d971f1d6c8f
dinhlongnguyen <dinhlongviolin1@gmail.com>
2022-01-28 14:49:44 +0700

    Notebook content update

--- taipy/gui/renderers/__init__.py
+++ taipy/gui/renderers/__init__.py
@@ -27,12 +27,18 @@ class PageRenderer(ABC):
 
         If `content` is a path to a readable file, the file is read entirely as the text template.
         """
-        self._content = None
+        self._content: t.Union[None, str] = None
+        self.__proceses_content(content)
+
+    def __proceses_content(self, content: str) -> None:
         if path.exists(content) and path.isfile(content):
             with open(t.cast(str, content), "r") as f:
                 self._content = f.read()
         else:
             self._content = content
+
+    def set_content(self, content: str) -> None:
+        self.__proceses_content(content)
 
     @abstractmethod
     def render(self) -> str:
