d34fb8fe62c47eda4fd68a7c4eef74715ff5ac40
dinhlongnguyen <dinhlongviolin1@gmail.com>
2022-02-28 00:17:47 +0700

    update requirements

--- setup.py
+++ setup.py
@@ -23,8 +23,7 @@ requirements = [
     "simple-websocket",
     "toml",
     "tzlocal",
-    "backports.zoneinfo;python_version<'3.9'",
-    "tzdata;platform_system=='Windows;",
+    "backports.zoneinfo[tzdata];python_version<'3.9'",
 ]
 
 test_requirements = ["pytest>=3.8"]
