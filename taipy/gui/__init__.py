f3228afb45b16cdc83a175da3fa97c908e362379
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Avaiga.com>
2022-03-04 10:53:44 +0100

    fix

--- taipy/gui/__init__.py
+++ taipy/gui/__init__.py
@@ -16,4 +16,4 @@ from .gui import Gui
 from .state import State
 from .icon import Icon
 from .renderers import Html, Markdown
-from .gui_actions import download, hold_actions, navigate, notify, resume_actions
+from .gui_actions import download, hold_control, navigate, notify, resume_control
