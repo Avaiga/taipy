0f30fc1a2685db5ab17cd22afce0b6d109be70c1
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-03-08 20:16:55 +0700

    Remove unidecode (#64)

--- taipy/core/common/_unicode_to_python_variable_name.py
+++
@@ -1,8 +0,0 @@
-import re
-
-import unidecode
-
-
-def _protect_name(name: str):
-    # get protect name by converting unicode characters to ASCII and removing special characters
-    return re.sub(r"[\W]+", "-", unidecode.unidecode(name).strip().lower().replace(" ", "_"))
