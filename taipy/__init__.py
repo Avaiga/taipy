e0e5442be6176b0ee10337d8a148850f64284d7e
Vuillemot Florian <florian.vuillemot@posteo.net>
2022-03-25 08:16:20 +0100

    Run GUI from REST server

--- taipy/__init__.py
+++ taipy/__init__.py
@@ -1,5 +1,12 @@
 from taipy.core import *
 import taipy.gui as gui
+import taipy.rest as rest
 
 __author__ = """Avaiga"""
 __email__ = "taipy.dev@avaiga.com"
+
+
+def run(page):
+    from taipy.rest.run import app
+
+    gui.Gui(flask=app).run()
