6e14410d5d5832d9e5fed1566dbba495dfed1470
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Avaiga.com>
2022-03-31 17:25:49 +0200

    make mypy (from GitHub) happy

--- taipy/gui/renderers/builder.py
+++ taipy/gui/renderers/builder.py
@@ -296,7 +296,7 @@ class _Builder:
             if len(lov) > 0:
                 for elt in lov:
                     ret = self.__gui._run_adapter(
-                        adapter, elt, adapter.__name__ if adapter else "adapter")  # type: ignore
+                        t.cast(t.Callable, adapter), elt, adapter.__name__ if adapter else "adapter")  # type: ignore
                     if ret is not None:
                         ret_list.append(ret)
             self.__attributes[f"default_{property_name}"] = ret_list
@@ -306,7 +306,7 @@ class _Builder:
             val_list = value if isinstance(value, list) else [value]
             for val in val_list:
                 ret = self.__gui._run_adapter(
-                    adapter, val, adapter.__name__ if adapter else "adapter", id_only=True)  # type: ignore
+                    t.cast(t.Callable, adapter), val, adapter.__name__ if adapter else "adapter", id_only=True)  # type: ignore
                 if ret is not None:
                     ret_list.append(ret)
             if multi_selection:
