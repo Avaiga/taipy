6eaf6dc5b5ccde8d4ed0708a5cc021ed881b43a7
toan-quach <toan.quach@avaiga.com>
2022-04-05 14:32:51 +0700

    added complex test with 10 tasks

--- tests/core/pipeline/test_pipeline.py
+++ tests/core/pipeline/test_pipeline.py
@@ -103,6 +103,7 @@ def test_get_sorted_tasks():
     #       |---> t1 ---|      -------------------------> t3 ---> s6
     #       |           |      |
     # s2 ---             ---> s4 ---> t4 ---> s7
+    print([[node.id for node in nodes] for nodes in pipeline._get_sorted_tasks()])
     assert pipeline._get_sorted_tasks() == [[task_1], [task_2, task_4], [task_3]]
 
     data_node_1 = DataNode("foo", Scope.PIPELINE, "s1")
@@ -183,7 +184,7 @@ def test_get_sorted_tasks():
     data_node_5 = DataNode("quux", Scope.PIPELINE, "s5")
     data_node_6 = DataNode("quuz", Scope.PIPELINE, "s6")
     data_node_7 = DataNode("corge", Scope.PIPELINE, "s7")
-    data_node_8 = DataNode("hugh", Scope.PIPELINE, "s7")
+    data_node_8 = DataNode("hugh", Scope.PIPELINE, "s8")
 
     task_1 = Task(
         "grault",
@@ -196,7 +197,7 @@ def test_get_sorted_tasks():
     task_3 = Task("waldo", print, [data_node_4], None, id=TaskId("t3"))
     task_4 = Task("fred", print, [data_node_4], [data_node_7], TaskId("t4"))
     task_5 = Task("bob", print, [data_node_8], None, TaskId("t5"))
-    pipeline = Pipeline("plugh", {}, [task_4, task_2, task_1, task_3, task_5], PipelineId("p1"))
+    pipeline = Pipeline("plugh", {}, [task_5, task_4, task_2, task_1, task_3], PipelineId("p1"))
     # s1 ---
     #       |
     #       |---> t1 ---|      -----> t3
@@ -204,7 +205,7 @@ def test_get_sorted_tasks():
     # s2 ---             ---> s4 ---> t4 ---> s7
     # t2 ---> s5
     # s8 ---> t5
-    assert pipeline._get_sorted_tasks() == [[task_2, task_1], [task_4, task_3], [task_5]]
+    assert pipeline._get_sorted_tasks() == [[task_5, task_2, task_1], [task_4, task_3]]
 
 
 def test_auto_set_and_reload(task):
