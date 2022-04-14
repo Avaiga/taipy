3b7fad17a146bb39b3bbe412164bf770f246fdbd
Vuillemot Florian <florian.vuillemot@posteo.net>
2022-04-14 19:42:50 +0200

    Feature/#27 release and publish (#153)
    * Release and publish in the same time
    
    * Ensure release version is equals to package version
    
    * Change environment name
    
    * Trigger release from action and not branch
    
    * Update version on the setup.py

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/avaiga/taipy-core",
-    version="1.0.0",
+    version="1.1.0.dev",
     zip_safe=False,
     extras_require=extras_require,
 )
