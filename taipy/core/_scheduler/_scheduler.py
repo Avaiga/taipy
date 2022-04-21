5471e43d791be66e1ee08be8f1cdba62b10e4cfd
Shiro <shiro@Shiros-MacBook-Pro.local>
2022-04-21 14:12:11 +0700

    change list() to []

--- taipy/core/_scheduler/_scheduler.py
+++ taipy/core/_scheduler/_scheduler.py
@@ -35,7 +35,7 @@ class _Scheduler(_AbstractScheduler):
     """
 
     jobs_to_run: Queue = Queue()
-    blocked_jobs: List = list()
+    blocked_jobs: List = []
     _dispatcher = _JobDispatcher(Config.job_config.nb_of_workers)  # type: ignore
     lock = Lock()
 
