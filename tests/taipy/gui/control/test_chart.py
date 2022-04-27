57f56057b0a779d87c490ac373f14e9175e67f5f
Fred Lefévère-Laoide <90181748+FredLL-Avaiga@users.noreply.github.com>
2022-04-27 10:52:36 +0200

    #153 support lat lon

--- tests/taipy/gui/control/test_chart.py
+++ tests/taipy/gui/control/test_chart.py
@@ -71,3 +71,35 @@ def test_chart_html_2(gui: Gui, helpers, csvdata):
     ]
     gui._set_frame(inspect.currentframe())
     helpers.test_control_html(gui, html_string, expected_list)
+
+def test_map_md(gui: Gui, helpers):
+    mapData = {
+        "Lat": [
+            48.4113, 18.0057, 48.6163, 48.5379, 48.5843, 48.612, 48.6286, 48.6068, 48.4489, 48.6548, 18.5721, 48.3734,
+            17.6398, 48.5765, 48.4407, 48.2286,
+        ],
+        "Lon": [
+            -112.8352, -65.804, -113.4784, -114.0702, -111.0188, -110.7939, -109.4629, -114.9123, -112.9705, -113.965,
+            -66.5401, -111.5245, -64.7246, -112.1932, -113.3159, -104.5863,
+        ],
+        "Globvalue": [
+            0.0875, 0.0892, 0.0908, 0.0933, 0.0942, 0.095, 0.095, 0.095, 0.0958, 0.0958, 0.0958, 0.0958, 0.0958, 0.0975,
+            0.0983, 0.0992,
+        ],
+    }
+    marker = { "color": "fuchsia", "size": 4 }
+    layout = {
+			"dragmode": "zoom",
+			"mapbox": { "style": "open-street-map", "center": { "lat": 38, "lon": -90 }, "zoom": 3 },
+			"margin": { "r": 0, "t": 0, "b": 0, "l": 0 }
+		}
+    md = "<|{mapData}|chart|type=scattermapbox|marker={marker}|layout={layout}|lat=Lat|lon=Lon|text=Globvalue|mode=markers|>"
+    gui._set_frame(inspect.currentframe())
+    expected_list = [
+        "<Chart",
+        'config="{&quot;columns&quot;: &#x7B;&quot;Globvalue&quot;: &#x7B;&quot;index&quot;: 0, &quot;type&quot;: &quot;float&quot;, &quot;dfid&quot;: &quot;Globvalue&quot;&#x7D;, &quot;Lat&quot;: &#x7B;&quot;index&quot;: 1, &quot;type&quot;: &quot;float&quot;, &quot;dfid&quot;: &quot;Lat&quot;&#x7D;, &quot;Lon&quot;: &#x7B;&quot;index&quot;: 2, &quot;type&quot;: &quot;float&quot;, &quot;dfid&quot;: &quot;Lon&quot;&#x7D;}, &quot;labels&quot;: [&quot;&quot;], &quot;texts&quot;: [&quot;Globvalue&quot;], &quot;modes&quot;: [&quot;markers&quot;], &quot;types&quot;: [&quot;scattermapbox&quot;], &quot;xaxis&quot;: [&quot;x&quot;], &quot;yaxis&quot;: [&quot;y&quot;], &quot;markers&quot;: [&#x7B;&quot;color&quot;: &quot;fuchsia&quot;, &quot;size&quot;: 4&#x7D;], &quot;selectedMarkers&quot;: [null], &quot;traces&quot;: [[&quot;Lat&quot;, &quot;Lon&quot;, null]], &quot;orientations&quot;: [null], &quot;names&quot;: [null], &quot;lines&quot;: [&#x7B;&quot;dash&quot;: null&#x7D;], &quot;textAnchors&quot;: [null], &quot;options&quot;: [null]}',
+        'data={_TpD_mapData}',
+        'layout="{&quot;dragmode&quot;: &quot;zoom&quot;, &quot;mapbox&quot;: &#x7B;&quot;style&quot;: &quot;open-street-map&quot;, &quot;center&quot;: &#x7B;&quot;lat&quot;: 38, &quot;lon&quot;: -90&#x7D;, &quot;zoom&quot;: 3&#x7D;, &quot;margin&quot;: &#x7B;&quot;r&quot;: 0, &quot;t&quot;: 0, &quot;b&quot;: 0, &quot;l&quot;: 0&#x7D;}"',
+        'updateVarName="_TpD_mapData"',
+    ]
+    helpers.test_control_md(gui, md, expected_list)
