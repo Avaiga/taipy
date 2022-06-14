c683dd9bb7c953e1734d3d050c1d2e7e63d79bc5
Jean-Robin <jeanrobin.medori@avaiga.com>
2022-06-14 17:40:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/avaiga/taipy-core",
-    version="1.1.0.dev",
+    version="1.1.0.dev1",
     zip_safe=False,
     extras_require=extras_require,
 )
