2e8cf1fc6b007bbe11d6a51fd01652d7f59de016
dinhlongnguyen <dinhlongviolin1@gmail.com>
2022-06-23 20:43:25 +0700

    bump verison to 1.2.0.dev0 (#307)

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/avaiga/taipy-gui",
-    version="1.1.0",
+    version="1.2.0.dev0",
     zip_safe=False,
     extras_require=extras_require,
     cmdclass={"build_py": NPMInstall},
