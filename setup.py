1cbff11c7a2a3797772d20e5aa0ba10a8f9faef7
dinhlongnguyen <dinhlongviolin1@gmail.com>
2022-09-30 19:44:53 +0700

    update taipy-config version (#431)

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ requirements = [
     "gevent>=21.12.0,<22.0",
     "gevent-websocket>=0.10.1,<0.11",
     "kthread>=0.2.3,<0.3",
-    "taipy-config@git+https://git@github.com/Avaiga/taipy-config.git@develop",
+    "taipy-config>=2.0,<3.0",
 ]
 
 test_requirements = ["pytest>=3.8"]
