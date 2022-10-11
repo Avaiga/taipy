459a0434da721b888cbe610f4e3ce69757272f2c
jrobinAV <88036007+jrobinAV@users.noreply.github.com>
2022-10-11 13:29:43 +0200

    Expose Scope and frequency at the right level

--- src/taipy/core/config/__init__.py
+++ src/taipy/core/config/__init__.py
@@ -11,6 +11,9 @@
 
 from taipy.config.checker._checker import _Checker
 from taipy.config.config import Config
+from taipy.config.common.frequency import Frequency
+from taipy.config.common.scope import Scope
+from taipy.config.global_app.global_app_config import GlobalAppConfig
 
 from .checkers._data_node_config_checker import _DataNodeConfigChecker
 from .checkers._job_config_checker import _JobConfigChecker
