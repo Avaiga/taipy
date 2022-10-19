974381b4b13a0a54d2bebb8bc7c94a38bbe0de84
Jean-Robin <jeanrobin.medori@avaiga.com>
2022-10-19 16:22:49 +0200

    Feature/handle section documentation (#15)
    * expose method to inject documentation with a section
    
    * expose method to inject documentation with a section

--- src/taipy/config/__init__.py
+++ src/taipy/config/__init__.py
@@ -8,6 +8,8 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
+import os
+from typing import List
 
 from .checker.issue import Issue
 from .checker.issue_collector import IssueCollector
@@ -17,3 +19,34 @@ from .config import Config
 from .global_app.global_app_config import GlobalAppConfig
 from .common.scope import Scope
 from .common.frequency import Frequency
+
+
+def _config_doc(func):
+    def func_with_doc(section, attribute_name, default, configuration_methods):
+        if "taipy-doc" in os.getcwd():
+            with open('config_doc.txt', 'a') as f:
+                from inspect import signature
+                for exposed_configuration_method, configuration_method in configuration_methods:
+                    annotation = "    @staticmethod\n"
+                    sign = "    def " + exposed_configuration_method + str(signature(configuration_method)) + ":\n"
+                    doc = '        """' + configuration_method.__doc__ + '"""\n'
+                    content = '        pass\n\n'
+                    f.write(annotation + sign + doc + content)
+        return func(section, attribute_name, default, configuration_methods)
+
+    return func_with_doc
+
+
+@_config_doc
+def _inject_section(section_clazz, attribute_name: str, default: Section, configuration_methods: List[tuple]):
+    Config._register_default(default)
+
+    if issubclass(section_clazz, UniqueSection):
+        setattr(Config, attribute_name, Config.unique_sections[section_clazz.name])
+    elif issubclass(section_clazz, Section):
+        setattr(Config, attribute_name, Config.sections[section_clazz.name])
+    else:
+        raise TypeError
+
+    for exposed_configuration_method, configuration_method in configuration_methods:
+        setattr(Config, exposed_configuration_method, configuration_method)
