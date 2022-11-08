fdc131562b755e8ed5a123bdcef06e6fe988face
jrobinAV <88036007+jrobinAV@users.noreply.github.com>
2022-11-08 17:26:33 +0100

    #255 add core package ref man

--- src/taipy/config/__init__.py
+++ src/taipy/config/__init__.py
@@ -8,6 +8,16 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
+
+"""# Taipy Config
+
+The Taipy Config package is a Python library designed to configure a Taipy application.
+
+The main entrypoint is the `Config^` singleton class. It exposes some methods to configure the
+Taipy application and some attributes to retrieve the configuration values.
+
+"""
+
 import os
 from typing import List
 
