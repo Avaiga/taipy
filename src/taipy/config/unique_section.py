9a6a8922140003cd85af580670b73ee8affb9a08
jrobinAV <88036007+jrobinAV@users.noreply.github.com>
2022-11-15 16:51:46 +0100

    minor fix on ref man

--- src/taipy/config/unique_section.py
+++ src/taipy/config/unique_section.py
@@ -5,7 +5,7 @@ from .common._validate_id import _validate_id
 
 
 class UniqueSection(Section, ABC):
-    """ A UniqueSection is a configuration Section^ that can have only one instance.
+    """ A UniqueSection is a configuration `Section^` that can have only one instance.
 
     A UniqueSection is only defined by the section name.
     """
