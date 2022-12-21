54af7db85f11d48a78286a8339914822ef6a9324
João André <88906996+joaoandre-avaiga@users.noreply.github.com>
2022-12-21 06:49:26 -0300

    Update src/taipy/config/config.pyi
    Co-authored-by: Đỗ Trường Giang <do.giang@avaiga.com>

--- src/taipy/config/config.pyi
+++ src/taipy/config/config.pyi
@@ -8,6 +8,19 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
+
+import json
+from typing import Any, Callable, Dict, List, Union, Optional
+
+from .checker.issue_collector import IssueCollector
+from .common._classproperty import _Classproperty
+from .common._config_blocker import _ConfigBlocker
+from .common.scope import Scope
+from .common.frequency import Frequency
+from .global_app.global_app_config import GlobalAppConfig
+from .section import Section
+from .unique_section import UniqueSection
+
 class Config:
     @_Classproperty
     def unique_sections(cls) -> Dict[str, UniqueSection]:
