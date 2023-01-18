59877746788233d9169290dd699563d660658032
Joao Andre <joao.dasilva@avaiga.com>
2023-01-18 18:25:47 -0300

    fix: add support for timedelta in CustomJsonEncoder/Decoder

--- tests/core/repository/test_json_encoder_decoder.py
+++ tests/core/repository/test_json_encoder_decoder.py
@@ -11,7 +11,7 @@
 
 import json
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytest
 
@@ -20,7 +20,12 @@ from src.taipy.core._repository._filesystem_repository import _CustomDecoder, _C
 
 @pytest.fixture(scope="function", autouse=True)
 def create_and_delete_json_file():
-    test_json_file = {"name": "testing", "date": datetime(1991, 1, 1), "default_data": "data for testing encoder"}
+    test_json_file = {
+        "name": "testing",
+        "date": datetime(1991, 1, 1),
+        "default_data": "data for testing encoder",
+        "validity_period": timedelta(days=1),
+    }
     with open("data.json", "w") as f:
         json.dump(test_json_file, f, ensure_ascii=False, indent=4, cls=_CustomEncoder)
     yield
@@ -32,7 +37,10 @@ def test_json_encoder():
         data = json.load(json_file)
         assert data["name"] == "testing"
         assert data["default_data"] == "data for testing encoder"
-        assert data["date"] == {"__type__": "Datetime", "__value__": "1991-01-01T00:00:00"}
+        assert data["date"] == {
+            "__type__": "Datetime",
+            "__value__": "1991-01-01T00:00:00",
+        }
         assert data["date"].get("__type__") == "Datetime"
         assert data["date"].get("__value__") == "1991-01-01T00:00:00"
 
