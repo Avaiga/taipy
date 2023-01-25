5e3d700af8252bc7e6333f50bef6a1168eb31af5
Joao Andre <joao.dasilva@avaiga.com>
2023-01-24 22:12:18 -0300

    chore: add python 3.11 support to setup.py

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     description="A 360° open-source platform from Python pilots to production-ready web apps.",
     long_description=readme,
