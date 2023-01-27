2eedb2c2bffe7ad1dd421c1f778b9481ff2c0d81
Jean-Robin <jeanrobin.medori@avaiga.com>
2023-01-27 10:53:30 +0100

    Attempt to fix tests due to file not found error. (#42)

--- src/taipy/config/version.py
+++ src/taipy/config/version.py
@@ -3,7 +3,7 @@ import os
 
 
 def _get_version():
-    with open(f"src{os.sep}taipy{os.sep}config{os.sep}version.json") as version_file:
+    with open(f"{os.path.dirname(os.path.abspath(__file__))}{os.sep}version.json") as version_file:
         version = json.load(version_file)
         version_string = f'{version.get("major", 0)}.{version.get("minor", 0)}.{version.get("patch", 0)}'
         if vext := version.get("ext"):
