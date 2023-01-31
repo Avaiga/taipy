18aab0f0a0e59f6ee36afd8bec825f8d03fb2477
jrobinAV <88036007+jrobinAV@users.noreply.github.com>
2023-01-31 08:54:57 +0100

    sort imports on init.py

--- src/taipy/__init__.py
+++ src/taipy/__init__.py
@@ -17,12 +17,46 @@ if find_spec("taipy"):
 
     if find_spec("taipy.core"):
         from taipy.core._core import Core
+        from taipy.core.common.alias import CycleId, DataNodeId, JobId, PipelineId, ScenarioId, TaskId
         from taipy.core.cycle.cycle import Cycle
         from taipy.core.data.data_node import DataNode
         from taipy.core.job.job import Job
         from taipy.core.job.status import Status
         from taipy.core.pipeline.pipeline import Pipeline
+        from taipy.core.scenario.scenario import Scenario
         from taipy.core.task.task import Task
+        from taipy.core.taipy import (
+            cancel_job,
+            clean_all_entities,
+            clean_all_entities_by_version,
+            compare_scenarios,
+            create_pipeline,
+            create_scenario,
+            delete,
+            delete_job,
+            delete_jobs,
+            export_scenario,
+            get,
+            get_cycles,
+            get_data_nodes,
+            get_jobs,
+            get_latest_job,
+            get_parents,
+            get_pipelines,
+            get_primary,
+            get_primary_scenarios,
+            get_scenarios,
+            get_tasks,
+            set,
+            set_primary,
+            submit,
+            subscribe_pipeline,
+            subscribe_scenario,
+            tag,
+            unsubscribe_pipeline,
+            unsubscribe_scenario,
+            untag,
+        )
 
     if find_spec("taipy.gui"):
         from taipy.gui import Gui
