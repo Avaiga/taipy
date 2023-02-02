4bd19e2dd1acb274618d66662764cd64e595c532
Irv Lustig <irv@princeton.com>
2023-02-02 12:55:51 -0500

    fix typing for taipy.run

--- src/taipy/_run.py
+++ src/taipy/_run.py
@@ -9,22 +9,32 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
+import sys
 import typing as t
+
+from flask import Flask
 
 from taipy.gui import Gui
 from taipy.rest import Rest
 from taipy.core import Core
 
+if sys.version_info < (3, 10):
+    from typing_extensions import TypeGuard
+else:
+    from typing import TypeGuard
 
-def _run(*apps: t.List[t.Union[Gui, Rest, Core]], **kwargs) -> t.Optional[t.Union[Gui, Rest, Core]]:
+
+def _run(*apps: t.Union[Gui, Rest, Core], **kwargs) -> t.Optional[t.Union[Gui, Rest, Core, Flask]]:
     """Run one or multiple Taipy services.
 
     A Taipy service is an instance of a class that runs code as a Web application.
 
     Parameters:
-        *args (List[Union[`Gui^`, `Rest^`, `Core^`]]): Services to run. If several services are provided, all the services run simultaneously. If this is empty or set to None, this method does nothing.
+        *args (Union[`Gui^`, `Rest^`, `Core^`]): Services to run, as separate arguments.
+            If several services are provided, all the services run simultaneously. If this is empty or set to None, this method does nothing.
         **kwargs: Other parameters to provide to the services.
     """
+
     gui = __typing_get(apps, Gui)
     rest = __typing_get(apps, Rest)
     core = __typing_get(apps, Core)
@@ -49,8 +59,15 @@ def _run(*apps: t.List[t.Union[Gui, Rest, Core]], **kwargs) -> t.Optional[t.Unio
         return gui.run(**kwargs)
     else:
         app = rest or gui
+        assert app is not None  # for pyright typing
         return app.run(**kwargs)
 
 
-def __typing_get(l, type_):
-    return next(filter(lambda o: isinstance(o, type_), l), None)
+TObj = t.TypeVar("TObj", bound=t.Union[Gui, Core, Rest])
+
+
+def __typing_get(tup_obj: t.Tuple[t.Union[Gui, Core, Rest], ...], type_: t.Type[TObj]) -> t.Optional[TObj]:
+    def filter_isinstance(tl: t.Union[Gui, Core, Rest]) -> TypeGuard[TObj]:
+        return isinstance(tl, type_)
+
+    return next(filter(filter_isinstance, tup_obj), None)
