c7613209f5ccfe5c1318a716eff3274273b5c696
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2023-02-20 17:46:51 +0100

    fix tests

--- gui/src/components/Taipy/Table.spec.tsx
+++ gui/src/components/Taipy/Table.spec.tsx
@@ -21,12 +21,12 @@ const tableColumns = JSON.stringify({"Entity": {"dfid": "Entity"}});
 
 describe("Table Component", () => {
     it("renders paginated", async () => {
-        const { getByText } = render(<Table data={undefined} columns={tableColumns}  />);
+        const { getByText } = render(<Table data={undefined} defaultColumns={tableColumns}  />);
         const elt = getByText("Entity");
         expect(elt.tagName).toBe("DIV");
     });
     it("renders auto loading", async () => {
-        const { getByText } = render(<Table data={undefined} columns={tableColumns} autoLoading={true} />);
+        const { getByText } = render(<Table data={undefined} defaultColumns={tableColumns} autoLoading={true} />);
         const elt = getByText("Entity");
         expect(elt.tagName).toBe("DIV");
     });
