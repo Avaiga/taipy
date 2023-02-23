ee2fc0460b0672e48471ff607651bb7c013dc790
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2023-02-23 19:06:02 +0100

    fix timezone tests on windows

--- tests/taipy/gui/e2e/test_timezone.py
+++ tests/taipy/gui/e2e/test_timezone.py
@@ -23,30 +23,30 @@ from taipy.gui.utils.date import _ISO_to_date
 
 @pytest.mark.teste2e
 def test_timzone_specified_1(page: "Page", gui: Gui, helpers):
-    _timezone_test_template(page, gui, helpers, "Etc/GMT", "2022-03-03 00:00:00 UTC")
+    _timezone_test_template(page, gui, helpers, "Etc/GMT", ["2022-03-03 00:00:00 UTC"])
 
 
 @pytest.mark.teste2e
 def test_timzone_specified_2(page: "Page", gui: Gui, helpers):
-    _timezone_test_template(page, gui, helpers, "Europe/Paris", "2022-03-03 01:00:00 GMT+1")
+    _timezone_test_template(page, gui, helpers, "Europe/Paris", ["2022-03-03 01:00:00 GMT+1", "2022-03-03 01:00:00 UTC+1"])
 
 
 @pytest.mark.teste2e
 def test_timzone_specified_3(page: "Page", gui: Gui, helpers):
-    _timezone_test_template(page, gui, helpers, "Asia/Ho_Chi_Minh", "2022-03-03 07:00:00 GMT+7")
+    _timezone_test_template(page, gui, helpers, "Asia/Ho_Chi_Minh", ["2022-03-03 07:00:00 GMT+7", "2022-03-03 07:00:00 UTC+7"])
 
 
 @pytest.mark.teste2e
 def test_timzone_specified_4(page: "Page", gui: Gui, helpers):
-    _timezone_test_template(page, gui, helpers, "America/Sao_Paulo", "2022-03-02 21:00:00 GMT-3")
+    _timezone_test_template(page, gui, helpers, "America/Sao_Paulo", ["2022-03-02 21:00:00 GMT-3", "2022-03-02 21:00:00 UTC−3"])
 
 
 @pytest.mark.teste2e
 def test_timezone_client_side(page: "Page", gui: Gui, helpers):
-    _timezone_test_template(page, gui, helpers, "client", "2022-03-03 01:00:00 GMT+1")
+    _timezone_test_template(page, gui, helpers, "client", ["2022-03-03 01:00:00 GMT+1", "2022-03-03 01:00:00 UTC+1"])
 
 
-def _timezone_test_template(page: "Page", gui: Gui, helpers, time_zone, text):
+def _timezone_test_template(page: "Page", gui: Gui, helpers, time_zone, texts):
     page_md = """
 <|{t}|id=text1|>
 """
@@ -58,4 +58,4 @@ def _timezone_test_template(page: "Page", gui: Gui, helpers, time_zone, text):
     page.expect_websocket()
     page.wait_for_selector("#text1")
     text1 = page.query_selector("#text1")
-    assert text1.inner_text() == text
+    assert text1.inner_text() in texts
