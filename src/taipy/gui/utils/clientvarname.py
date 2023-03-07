2b119c952d3382b799f4673bc02845be588de8f1
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2023-03-07 11:45:44 +0100

    Fab's comment

--- src/taipy/gui/utils/clientvarname.py
+++ src/taipy/gui/utils/clientvarname.py
@@ -9,8 +9,6 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-import typing as t
-
 _replace_dict = {".": "__", "[": "_SqrOp_", "]": "_SqrCl_"}
 
 def _get_client_var_name(s: str) -> str:
