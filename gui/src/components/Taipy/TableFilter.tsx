bd3a2b514f28a1a96fa3d2bab5d247a12e7a8bb1
Fred Lefévère-Laoide <90181748+FredLL-Avaiga@users.noreply.github.com>
2023-03-20 14:08:53 +0100

    #667 reset applied filters (#670)
    Co-authored-by: Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
    Co-authored-by: dinhlongnguyen <dinhlongviolin1@gmail.com>

--- gui/src/components/Taipy/TableFilter.tsx
+++ gui/src/components/Taipy/TableFilter.tsx
@@ -322,7 +322,7 @@ const TableFilter = (props: TableFilterProps) => {
     }, []);
 
     const onApply = useCallback(() => {
-        onValidate(filters);
+        onValidate([...filters]);
         onShowFilterClick();
     }, [onValidate, filters, onShowFilterClick]);
     const onRemove = useCallback(() => {
@@ -333,7 +333,13 @@ const TableFilter = (props: TableFilterProps) => {
     return (
         <>
             <Tooltip title="Filter list">
-                <IconButton onClick={onShowFilterClick} size="small" ref={filterRef} sx={iconInRowSx} className={getSuffixedClassNames(className, "-filter-icon")}>
+                <IconButton
+                    onClick={onShowFilterClick}
+                    size="small"
+                    ref={filterRef}
+                    sx={iconInRowSx}
+                    className={getSuffixedClassNames(className, "-filter-icon")}
+                >
                     <FilterListIcon fontSize="inherit" />
                 </IconButton>
             </Tooltip>
@@ -371,15 +377,15 @@ const TableFilter = (props: TableFilterProps) => {
                             variant="outlined"
                             color="inherit"
                         >
-                            {`Reset list (remove applied filter${filters.length > 1 ? "s" : ""})`}
+                            {`Reset list (remove applied filter${appliedFilters.length > 1 ? "s" : ""})`}
                         </Button>
-                        <Button 
-                            endIcon={<SendIcon />} 
-                            onClick={onApply} 
-                            disabled={filters.length == 0} 
+                        <Button
+                            endIcon={<SendIcon />}
+                            onClick={onApply}
+                            disabled={filters.length == 0}
                             variant="outlined"
                         >
-                            {`Apply ${ filters.length } filter${filters.length > 1 ? "s" : ""}`}
+                            {`Apply ${filters.length} filter${filters.length > 1 ? "s" : ""}`}
                         </Button>
                     </Box>
                 </Box>
