c098bb56db4bbc4ab653538f8fd6ba489b386325
Jaufrey Lafuste <115568883+JaufreyTpy@users.noreply.github.com>
2023-04-12 18:35:36 +0200

    Feature/#699 add root margin variable (#710)
    * feat(stylekit): add root_margin stylekit variable
    
    * feat(stylekit): add todo to transfer the run() parameter "margin" value in rootr_margin variable
    
    * feat(stylekit): remove forced margin 0 on Root element in css
    
    * feat(stylekit): assign --root-margin CSS property on root element
    
    * resolve stylekit margin
    
    ---------
    
    Co-authored-by: Jaufrey Lafuste <jaufrey.lafuste@winzana.com>
    Co-authored-by: dinhlongnguyen <dinhlongviolin1@gmail.com>

--- src/taipy/gui/config.py
+++ src/taipy/gui/config.py
@@ -75,10 +75,11 @@ Stylekit = t.TypedDict(
         "color_background_dark": str,
         "color_paper_dark": str,
         "font_family": str,
+        "root_margin": str,
         "border_radius": int,
-        "input_button_height": str
+        "input_button_height": str,
     },
-    total=False
+    total=False,
 )
 
 Config = t.TypedDict(
@@ -122,7 +123,6 @@ Config = t.TypedDict(
 
 
 class _Config(object):
-
     __RE_PORT_NUMBER = re.compile(
         r"^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$"
     )
@@ -205,6 +205,7 @@ class _Config(object):
                 # Special case for "stylekit" that can be a Boolean or a dict
                 if key == "stylekit" and isinstance(value, bool):
                     from ._default_config import _default_stylekit
+
                     config[key] = _default_stylekit if value else {}
                     continue
                 try:
@@ -273,3 +274,21 @@ class _Config(object):
                 logger,
                 "'async_mode' parameter has been overridden to 'threading'. Using Flask built-in development server with debug mode",
             )
+
+        self._resolve_stylekit()
+
+    def _resolve_stylekit(self):
+        app_config = self.config
+        # support legacy margin variable
+        stylekit_config = app_config["stylekit"]
+
+        if isinstance(app_config["stylekit"], dict) and "root_margin" in app_config["stylekit"]:
+            from ._default_config import _default_stylekit, default_config
+
+            stylekit_config = app_config["stylekit"]
+            if (
+                stylekit_config["root_margin"] == _default_stylekit["root_margin"]
+                and app_config["margin"] != default_config["margin"]
+            ):
+                app_config["stylekit"]["root_margin"] = str(app_config["margin"])
+            app_config["margin"] = None
