d46fd7c57aa603641f8cb6be611467959e70635b
trgiangdo <dtr.giang.1299@gmail.com>
2023-04-21 15:13:35 +0700

    feat: add ability to create subcommands with taipy.config._cli._Argparser

---
+++ src/taipy/config/_cli/_argparser.py
@@ -0,0 +1,54 @@
+# Copyright 2023 Avaiga Private Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+import argparse
+from typing import Dict
+
+
+class _Argparser:
+    """Argument parser for Taipy application."""
+
+    # The conflict_handler is set to "resolve" to override conflict arguments
+    _main_parser = argparse.ArgumentParser(conflict_handler="resolve", exit_on_error=False)
+    _subparser_action = _main_parser.add_subparsers()
+
+    sub_taipyparsers: Dict[str, argparse.ArgumentParser] = {}
+
+    @classmethod
+    def _add_subparser(cls, name: str, **kwargs) -> argparse.ArgumentParser:
+        """Create a new subparser and return a argparse .handle."""
+        try:
+            return cls.sub_taipyparsers[name]
+        except KeyError:
+            subparser = cls._subparser_action.add_parser(
+                name=name,
+                conflict_handler="resolve",
+                exit_on_error=False,
+                **kwargs,
+            )
+            cls.sub_taipyparsers[name] = subparser
+            subparser.set_defaults(which=name)
+            return subparser
+
+    @classmethod
+    def _parse(cls):
+        """Parse and return only known arguments."""
+        args, _ = cls._main_parser.parse_known_args()
+        return args
+
+    @classmethod
+    def _remove_subparser(cls, name: str):
+        """Remove a subparser from argparse."""
+        cls._subparser_action._name_parser_map.pop(name, None)
+
+        for action in cls._subparser_action._choices_actions:
+            if action.dest == name:
+                cls._subparser_action._choices_actions.remove(action)
