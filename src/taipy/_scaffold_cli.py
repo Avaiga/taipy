7d982c203a8f278bb2db5cc995697852b4bd751d
trgiangdo <dtr.giang.1299@gmail.com>
2023-04-21 15:42:01 +0700

    feat: add _ScaffoldCLI to create project from scaffold by cookiecutter

---
+++ src/taipy/_scaffold_cli.py
@@ -0,0 +1,40 @@
+# Copyright 2023 Avaiga Private Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+import sys
+
+from cookiecutter.main import cookiecutter
+
+from taipy.config._cli._argparser import _Argparser
+
+
+class _ScaffoldCLI:
+    __TEMPLATE_MAP = {
+        "default": "default_template_path",
+    }
+
+    @classmethod
+    def create_parser(cls):
+        create_parser = _Argparser._add_subparser("create", help="Create a new Taipy application.")
+        create_parser.add_argument(
+            "--template",
+            choices=["default", "scenario_manager"],
+            default="default",
+            help="The Taipy template to create new application.",
+        )
+
+    @classmethod
+    def parse_arguments(cls):
+        args = _Argparser._parse()
+
+        if getattr(args, "which", None) == "create":
+            cookiecutter(cls.__TEMPLATE_MAP[args.template])
+            sys.exit(0)
