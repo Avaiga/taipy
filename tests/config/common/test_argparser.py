86a7b1bb697634e8f26d7d0c74b18801cf499032
trgiangdo <dtr.giang.1299@gmail.com>
2023-04-21 15:14:44 +0700

    test: update test for subcommands

--- tests/config/common/test_argparser.py
+++ tests/config/common/test_argparser.py
@@ -9,57 +9,102 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-import argparse
+import re
 
 import pytest
 
-from src.taipy.config.common._argparser import _Argparser
+from src.taipy.config._cli._argparser import _Argparser
+
+
+def preprocess_stdout(stdout):
+    stdout = stdout.replace("\n", " ").replace("\t", " ")
+    return re.sub(" +", " ", stdout)
 
 
 @pytest.fixture(autouse=True, scope="function")
 def clean_argparser():
-    _Argparser.parser = argparse.ArgumentParser(conflict_handler="resolve")
-    _Argparser.arg_groups = {}
+    _Argparser.sub_taipyparsers = {}
 
     yield
 
 
 def test_parser(capfd):
-    group_1 = _Argparser._add_groupparser("group_1", "group_1 desc")
-    group_1.add_argument("--foo", "-f", help="foo help")
-    group_1.add_argument("--bar", "-b", help="bar help")
+    subcommand_1 = _Argparser._add_subparser("subcommand_1", help="subcommand_1 help")
+    subcommand_1.add_argument("--foo", "-f", help="foo help")
+    subcommand_1.add_argument("--bar", "-b", help="bar help")
 
-    group_2 = _Argparser._add_groupparser("group_2", "group_2 desc")
-    group_2.add_argument("--doo", "-d", help="doo help")
-    group_2.add_argument("--baz", "-z", help="baz help")
+    subcommand_2 = _Argparser._add_subparser("subcommand_2", help="subcommand_2 help")
+    subcommand_2.add_argument("--doo", "-d", help="doo help")
+    subcommand_2.add_argument("--baz", "-z", help="baz help")
 
-    expected_help_message = """
-group_1:
-  group_1 desc
+    expected_help_message = """[-h] {subcommand_1,subcommand_2} ...
 
+positional arguments:
+  {subcommand_1,subcommand_2}
+    subcommand_1        subcommand_1 help
+    subcommand_2        subcommand_2 help
+
+options:
+  -h, --help            show this help message and exit
+    """.strip()
+
+    _Argparser._main_parser.print_help()
+    stdout, _ = capfd.readouterr()
+    assert preprocess_stdout(expected_help_message) in preprocess_stdout(stdout)
+
+    expected_subcommand_1_help_message = """subcommand_1 [-h] [--foo FOO] [--bar BAR]
+
+options:
+  -h, --help         show this help message and exit
   --foo FOO, -f FOO  foo help
   --bar BAR, -b BAR  bar help
+    """.strip()
 
-group_2:
-  group_2 desc
+    subcommand_1.print_help()
+    stdout, _ = capfd.readouterr()
+    assert preprocess_stdout(expected_subcommand_1_help_message) in preprocess_stdout(stdout)
 
+    expected_subcommand_2_help_message = """subcommand_2 [-h] [--doo DOO] [--baz BAZ]
+
+options:
+  -h, --help         show this help message and exit
   --doo DOO, -d DOO  doo help
   --baz BAZ, -z BAZ  baz help
     """.strip()
 
-    _Argparser.parser.print_help()
+    subcommand_2.print_help()
     stdout, _ = capfd.readouterr()
+    assert preprocess_stdout(expected_subcommand_2_help_message) in preprocess_stdout(stdout)
 
-    assert expected_help_message in stdout
+
+def test_duplicate_subcommand():
+    subcommand_1 = _Argparser._add_subparser("subcommand_1", help="subcommand_1 help")
+    subcommand_1.add_argument("--foo", "-f", help="foo help")
+
+    subcommand_2 = _Argparser._add_subparser("subcommand_1", help="subcommand_2 help")
+    subcommand_2.add_argument("--bar", "-b", help="bar help")
+
+    # The title of subcommand_2 is duplicated with  subcommand_1, and therefore
+    # there will be no new subcommand created
+    assert len(_Argparser.sub_taipyparsers) == 1
 
 
-def test_duplicate_group(capfd):
-    group_1 = _Argparser._add_groupparser("group_1", "group_1 desc")
-    group_1.add_argument("--foo", "-f", help="foo help")
+def test_remove_subcommand(capfd):
+    _Argparser._add_subparser("subcommand_1", help="subcommand_1 help")
+    _Argparser._add_subparser("subcommand_2", help="subcommand_2 help")
 
-    group_2 = _Argparser._add_groupparser("group_1", "group_2 desc")
-    group_2.add_argument("--bar", "-b", help="bar help")
+    _Argparser._remove_subparser("subcommand_1")
+    _Argparser._remove_subparser("subcommand_2")
 
-    # The title of group_2 is duplicated with  group_1, and therefore
-    # there will be no new group created
-    assert len(_Argparser.arg_groups) == 1
+    expected_help_message = """[-h] {} ...
+
+positional arguments:
+  {}
+
+options:
+  -h, --help  show this help message and exit
+    """.strip()
+
+    _Argparser._main_parser.print_help()
+    stdout, _ = capfd.readouterr()
+    assert preprocess_stdout(expected_help_message) in preprocess_stdout(stdout)
