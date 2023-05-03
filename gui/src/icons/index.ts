2ea76f024dae9258f93bde6a8b6040984c9b2820
Jaufrey Lafuste <jaufrey.lafuste@winzana.com>
2023-05-03 16:04:35 +0200

    feat(icons): add taipycons

---
+++ gui/src/icons/index.ts
@@ -0,0 +1,6 @@
+export { CycleIcon } from "./cycle-icon.component";
+export { DatanodeIcon } from "./datanode-icon.component";
+export { JobIcon } from "./job-icon.component";
+export { PipelineIcon } from "./pipeline-icon.component";
+export { ScenarioIcon } from "./scenario-icon.component";
+export { TaskIcon } from "./task-icon.component";
