ac5ced27fb985e7005541b2c1cbd8e822cf57d70
Toan Quach <93168955+toan-quach@users.noreply.github.com>
2023-05-22 13:38:39 +0700

    Update src/taipy/core/_backup/_backup.py
    Co-authored-by: Vuillemot Florian <florian.vuillemot@posteo.net>

--- src/taipy/core/_backup/_backup.py
+++ src/taipy/core/_backup/_backup.py
@@ -24,15 +24,22 @@ def append_to_backup_file(new_file_path: str):
 
 def remove_from_backup_file(to_remove_file_path: str):
     preserve_file_path = os.getenv(__BACKUP_FILE_PATH_ENVIRONMENT_VARIABLE_NAME, None)
-    if preserve_file_path and pathlib.Path(preserve_file_path).is_file():
-        with open(preserve_file_path, "r+") as f:
-            lines = f.readlines()
-            to_remove_file_path = f"{to_remove_file_path}\n"
-            if to_remove_file_path in lines:
-                f.seek(0)
-                f.truncate()
-                lines.remove(to_remove_file_path)
-                f.writelines(lines)
+    if preserve_file_path:
+       try: # The file will mostly be present, so testing if the file exists will cost you more than a simple test.
+          with open(preserve_file_path, "r+") as f:
+              old_backup = f.read()  # No parsing needs
+              to_remove_file_path = to_remove_file_path + "\n"
+              # If to_remove_file_path can be several times in the string
+              new_backup = old_backup.replace(to_remove_file_path, '')
+              # otherwise
+              new_backup = old_backup.replace(to_remove_file_path, '', 1)
+              if new_backup is not old_backup:
+                  f.seek(0)
+                  f.write(new_backup)
+                  # To verify, but I think it is better in that way.
+                  # The idea is to write new content on top of the current content then resize the file.
+                  f.truncate()
+         except: ....
 
 
 def replace_in_backup_file(old_file_path: str, new_file_path: str):
