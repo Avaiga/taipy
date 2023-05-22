8c684707f34ff02ab340a25c61f768b81abc72cd
Shiro <shiro@Shiros-MacBook-Pro.local>
2023-05-22 15:47:37 +0700

    added comments

--- src/taipy/core/_backup/_backup.py
+++ src/taipy/core/_backup/_backup.py
@@ -29,10 +29,14 @@ def remove_from_backup_file(to_remove_file_path: str):
             with open(preserve_file_path, "r+") as f:
                 old_backup = f.read()
                 to_remove_file_path = to_remove_file_path + "\n"
+
+                # To avoid removing the file path of different data nodes that are pointing
+                # to the same file. We will only replace the file path only once.
                 if old_backup.startswith(to_remove_file_path):
                     new_backup = old_backup.replace(to_remove_file_path, "", 1)
                 else:
                     new_backup = old_backup.replace("\n" + to_remove_file_path, "\n", 1)
+
                 if new_backup is not old_backup:
                     f.seek(0)
                     f.write(new_backup)
