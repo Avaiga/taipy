b909f5559f16bcf3921d8f71677f8f08e9b5e123
Fred Lefévère-Laoide <90181748+FredLL-Avaiga@users.noreply.github.com>
2023-05-23 16:50:21 +0200

    #154 improve dag looks (#155)
    Co-authored-by: Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>

--- gui/src/icons/input.tsx
+++ gui/src/icons/input.tsx
@@ -1,7 +1,11 @@
 import { SvgIcon, SvgIconProps } from "@mui/material";
 
 export const Input = (props: SvgIconProps) => (
-  <SvgIcon {...props} viewBox="0 0 24 24">
-    <path d="M 5,5 H 19 V 19 H 5 V 17 H 3 v 2 c 0,1.1 0.9,2 2,2 h 14 c 1.1,0 2,-0.9 2,-2 V 5 C 21,3.9 20.1,3 19,3 H 5 C 3.9,3 3,3.9 3,5 V 7 H 5 Z M 9.6,8.41 12.2,11 H 3 v 2 h 9.2 L 9.6,15.6 11,17 16,12 11,7 Z" />
-  </SvgIcon>
+    <SvgIcon {...props} viewBox="0 0 16 16">
+        <g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
+            <path d="m12.25 7.81h-5.41" fill="none" />
+            <circle cx="4.44" cy="7.79" r="1.2" fill="currentColor" />
+            <path d="m10.4 5.43 2.36 2.36-2.36 2.36" fill="none" />
+        </g>
+    </SvgIcon>
 );
