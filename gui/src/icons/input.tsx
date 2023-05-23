9958a792389c3612415a50aa9c2d74d40d97f761
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2023-05-23 17:11:23 +0200

    #154 input vs output

--- gui/src/icons/input.tsx
+++ gui/src/icons/input.tsx
@@ -3,9 +3,9 @@ import { SvgIcon, SvgIconProps } from "@mui/material";
 export const Input = (props: SvgIconProps) => (
     <SvgIcon {...props} viewBox="0 0 16 16">
         <g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
-            <path d="m12.25 7.81h-5.41" fill="none" />
-            <circle cx="4.44" cy="7.79" r="1.2" fill="currentColor" />
-            <path d="m10.4 5.43 2.36 2.36-2.36 2.36" fill="none" />
+            <path d="m8.8 8.02h-5.6" fill="none" />
+            <circle cx="11.71" cy="8" r="1.09" fill="currentColor" />
+            <path d="m7.12 5.85 2.15 2.15-2.15 2.15" fill="none" />
         </g>
     </SvgIcon>
 );
