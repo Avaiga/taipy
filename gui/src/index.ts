9593b2b2098de8bd362be23f0b869d215ddcf4cb
Pavle Marceta <pavle.marceta@avaiga.com>
2023-05-31 20:31:01 +0800

    fix: Removed unused import

--- gui/src/index.ts
+++ gui/src/index.ts
@@ -1,7 +1,6 @@
 import ScenarioSelector from "./ScenarioSelector";
 import ScenarioViewer from "./ScenarioViewer";
 import ScenarioDag from "./ScenarioDag";
-import ScenarioGraph from "./ScenarioGraph";
 import DataNodeExplorer from "./DataNodeExplorer";
 
-export { ScenarioSelector, ScenarioGraph as Graph, DataNodeExplorer, ScenarioDag as Dag };
+export { ScenarioSelector, DataNodeExplorer, ScenarioDag as Dag };
