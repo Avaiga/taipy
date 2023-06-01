d59a9173159e46fc42a2a8b0cc76146af858f9e8
Đỗ Trường Giang <do.giang@avaiga.com>
2023-06-02 06:32:06 +0700

    Feature/Doc#457 - Clarify the explanation of the `validity_period` attribute (#608)
    * doc: clarify the explaination of validity_period attribute
    
    * fix: linting error on line too long
    
    ---------
    
    Co-authored-by: jrobinAV <88036007+jrobinAV@users.noreply.github.com>

--- src/taipy/core/data/data_node.py
+++ src/taipy/core/data/data_node.py
@@ -64,9 +64,11 @@ class DataNode(_Entity, _Labeled):
             edition of that node.
         version (str): The string indicates the application version of the data node to
             instantiate. If not provided, the current version is used.
-        validity_period (Optional[timedelta]): The validity period of a data node.
-            Implemented as a timedelta. If *validity_period* is set to None, the data node is
-            always up-to-date.
+        validity_period (Optional[timedelta]): The duration implemented as a timedelta since the last edit date for
+            which the data node can be considered up-to-date. Once the validity period has passed, the data node is
+            considered stale and relevant tasks will run even if they are skippable (see the
+            [Task management page](https://docs.taipy.io/en/latest/manuals/core/entities/task-mgt) for more details).
+            If _validity_period_ is set to `None`, the data node is always up-to-date.
         edit_in_progress (bool): True if the data node is locked for modification. False
             otherwise.
         kwargs: A dictionary of additional properties.
