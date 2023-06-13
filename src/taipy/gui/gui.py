21e75a3825de607635658b988778983929f012ac
Fred Lefévère-Laoide <90181748+FredLL-Avaiga@users.noreply.github.com>
2023-06-13 09:04:07 +0200

    get_flask_app() protected against too early invocation (#789) (#790)
    * #789 get_flask_app
    
    * fab's comments
    
    ---------
    
    Co-authored-by: Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>

--- src/taipy/gui/gui.py
+++ src/taipy/gui/gui.py
@@ -1681,10 +1681,14 @@ class Gui:
     def get_flask_app(self) -> Flask:
         """Get the internal Flask application.
 
+        This method must be called **after** (Gui.)run^ method was invoked.
+
         Returns:
             The Flask instance used.
         """
-        return self._server.get_flask()
+        if hasattr(self, "_server"):
+            return self._server.get_flask()
+        raise RuntimeError("get_flask_app() cannot be invoked before run() has been called.")
 
     def _set_frame(self, frame: FrameType):
         if not isinstance(frame, FrameType):  # pragma: no cover
