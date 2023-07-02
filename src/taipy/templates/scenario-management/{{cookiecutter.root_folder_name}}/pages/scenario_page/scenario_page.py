48e1f810fcbf759eca6780a8d6466f56aa8354fd
jrobinAV <88036007+jrobinAV@users.noreply.github.com>
2023-07-02 20:44:22 +0200

    Remove useless code

--- src/taipy/templates/scenario-management/{{cookiecutter.root_folder_name}}/pages/scenario_page/scenario_page.py
+++ src/taipy/templates/scenario-management/{{cookiecutter.root_folder_name}}/pages/scenario_page/scenario_page.py
@@ -1,16 +1,4 @@
-from taipy.gui import Markdown, notify
-
-
-def on_clean_data(state):
-    state.scenario.submit(wait=True)
-    state.results = state.scenario.cleaned_dataset.read()
-    notify(state, message=f"csv file has been cleaned for scenario {state.scenario.get_simple_label()}`")
-
-
-def drop_csv(state):
-    state.scenario.initial_dataset.path = state.input_csv_file
-    state.inputs = state.scenario.initial_dataset.read()
-    notify(state, message=f"csv file `{state.input_csv_file}` loaded for scenario `{state.scenario.get_simple_label()}`")
+from taipy.gui import Markdown
 
 
 scenario_page = Markdown("pages/scenario_page/scenario_page.md")
