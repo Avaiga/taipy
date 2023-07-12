aaa6d91246eae089ff0a03cdd5557d933087d8be
jrobinAV <88036007+jrobinAV@users.noreply.github.com>
2023-07-12 19:41:55 +0200

    If the execution mode is not development or standalone, try to build an enterprise mode if enterprise is installed

--- src/taipy/core/_orchestrator/_orchestrator_factory.py
+++ src/taipy/core/_orchestrator/_orchestrator_factory.py
@@ -25,7 +25,7 @@ class _OrchestratorFactory:
     _TAIPY_ENTERPRISE_MODULE = "taipy.enterprise"
     _TAIPY_ENTERPRISE_CORE_ORCHESTRATOR_MODULE = _TAIPY_ENTERPRISE_MODULE + ".core._orchestrator._orchestrator"
     _TAIPY_ENTERPRISE_CORE_DISPATCHER_MODULE = _TAIPY_ENTERPRISE_MODULE + ".core._orchestrator._dispatcher"
-    __STANDADLONE_JOB_DISPATCHER_TYPE = "_StandaloneJobDispatcher"
+    __TAIPY_ENTERPRISE_BUILD_DISPATCHER_METHOD = "_build_dispatcher"
 
     _orchestrator: Optional[_Orchestrator] = None
     _dispatcher: Optional[_JobDispatcher] = None
@@ -49,48 +49,50 @@ class _OrchestratorFactory:
         if not cls._orchestrator:
             raise OrchestratorNotBuilt
         if Config.job_config.is_standalone:
-            return cls.__build_standalone_job_dispatcher(force_restart=force_restart)
+            cls.__build_standalone_job_dispatcher(force_restart=force_restart)
         elif Config.job_config.is_development:
-            return cls.__build_development_job_dispatcher()
+            cls.__build_development_job_dispatcher()
+        elif util.find_spec(cls._TAIPY_ENTERPRISE_MODULE) is not None:
+            cls.__build_enterprise_job_dispatcher(force_restart)
         else:
             raise ModeNotAvailable(f"Job mode {Config.job_config.mode} is not available.")
+        return cls._dispatcher
 
     @classmethod
     def _remove_dispatcher(cls) -> Optional[_JobDispatcher]:
-        if isinstance(cls._dispatcher, _StandaloneJobDispatcher) and not isinstance(
-            cls._dispatcher, _DevelopmentJobDispatcher
-        ):
+        if cls._dispatcher is not None and not isinstance(cls._dispatcher, _DevelopmentJobDispatcher):
             cls._dispatcher.stop()
-
         cls._dispatcher = None
-
         return cls._dispatcher
 
     @classmethod
-    def __build_standalone_job_dispatcher(cls, force_restart=False) -> Optional[_JobDispatcher]:
-        if isinstance(cls._dispatcher, _StandaloneJobDispatcher) and not isinstance(
-            cls._dispatcher, _DevelopmentJobDispatcher
-        ):
+    def __build_standalone_job_dispatcher(cls, force_restart=False):
+        if isinstance(cls._dispatcher, _StandaloneJobDispatcher):
             if force_restart:
                 cls._dispatcher.stop()
             else:
-                return None
+                return
 
         if util.find_spec(cls._TAIPY_ENTERPRISE_MODULE) is not None:
             cls._dispatcher = _load_fct(
-                cls._TAIPY_ENTERPRISE_CORE_DISPATCHER_MODULE, cls.__STANDADLONE_JOB_DISPATCHER_TYPE
+                cls._TAIPY_ENTERPRISE_CORE_DISPATCHER_MODULE, cls.__TAIPY_ENTERPRISE_BUILD_DISPATCHER_METHOD
             )(cls._orchestrator)
         else:
             cls._dispatcher = _StandaloneJobDispatcher(cls._orchestrator)  # type: ignore
-
         cls._dispatcher.start()  # type: ignore
-        return cls._dispatcher
 
     @classmethod
-    def __build_development_job_dispatcher(cls) -> _JobDispatcher:
-        if isinstance(cls._dispatcher, _StandaloneJobDispatcher) and not isinstance(
-            cls._dispatcher, _DevelopmentJobDispatcher
-        ):
+    def __build_development_job_dispatcher(cls):
+        if isinstance(cls._dispatcher, _StandaloneJobDispatcher):
             cls._dispatcher.stop()
         cls._dispatcher = _DevelopmentJobDispatcher(cls._orchestrator)  # type: ignore
-        return cls._dispatcher
+
+    @classmethod
+    def __build_enterprise_job_dispatcher(cls, force_restart=False):
+        cls._dispatcher = _load_fct(
+            cls._TAIPY_ENTERPRISE_CORE_DISPATCHER_MODULE, cls.__TAIPY_ENTERPRISE_BUILD_DISPATCHER_METHOD
+        )(cls._orchestrator, force_restart)
+        if cls._dispatcher:
+            cls._dispatcher.start()
+        else:
+            raise ModeNotAvailable(f"Job mode {Config.job_config.mode} is not available.")
