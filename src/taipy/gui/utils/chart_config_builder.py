c8d35914cdef271dd31558f3684350152922bf02
Fabien Lelaquais <86590727+FabienLelaquais@users.noreply.github.com>
2023-07-20 12:11:35 +0200

    Add support for the 'waterfall' chart type (#857)
    * Add 'measure' property for Waterfall charts, and add examples.
    * Document the new 'measure' property.

--- src/taipy/gui/utils/chart_config_builder.py
+++ src/taipy/gui/utils/chart_config_builder.py
@@ -55,6 +55,7 @@ class _Chart_iprops(Enum):
     values = 28
     labels = 29
     decimator = 30
+    measure = 31
 
 
 __CHART_AXIS: t.Dict[str, t.Tuple[_Chart_iprops, ...]] = {
@@ -74,6 +75,7 @@ __CHART_AXIS: t.Dict[str, t.Tuple[_Chart_iprops, ...]] = {
     "pie": (_Chart_iprops.values, _Chart_iprops.labels),
     "choropleth": (_Chart_iprops.locations, _Chart_iprops.z),
     "funnelarea": (_Chart_iprops.values,),
+    "waterfall": (_Chart_iprops.x, _Chart_iprops.y, _Chart_iprops.measure),
 }
 __CHART_DEFAULT_AXIS: t.Tuple[_Chart_iprops, ...] = (_Chart_iprops.x, _Chart_iprops.y, _Chart_iprops.z)
 __CHART_MARKER_TO_COLS: t.Tuple[str, ...] = ("color", "size", "symbol", "opacity")
