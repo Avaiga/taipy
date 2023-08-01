042b9f465e87f242662ce640765228224b62e042
Toan Quach <shiro@Shiros-MacBook-Pro.local>
2023-08-01 15:59:13 +0700

    make linter happy

--- src/taipy/core/exceptions/exceptions.py
+++ src/taipy/core/exceptions/exceptions.py
@@ -80,8 +80,8 @@ class ExposedTypeLengthMismatch(Exception):
 
 
 class SheetNameLengthMismatch(Exception):
-    """Raised if length of sheet_name list does not match with
-    number of sheets in the data to be written to Excel file."""
+    """Raised if length of sheet_name list does not match
+    with number of sheets in the data to be written to Excel file."""
 
 
 class InvalidExposedType(Exception):
