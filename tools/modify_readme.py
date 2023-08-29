4b475d95f21be5e003a51e629d906e7a729d810a
dinhlongnguyen <dinhlongviolin1@gmail.com>
2023-08-29 13:09:20 +0700

    modify readme as a file

---
+++ tools/modify_readme.py
@@ -0,0 +1,14 @@
+import sys
+import re
+
+repo_name = sys.argv[1]
+branch_name = sys.argv[2]
+pattern = re.compile("<img\\s+([^>]*?)(?<![\'\"])(?<!\\/)src\\s*=\\s*([\'\"])(?!http|\\/)(.*?)\\2([^>]*?)>")
+replacement = r'<img \1src="https://raw.githubusercontent.com/Avaiga/{repo_name}/{branch_name}/\3"\4>'
+
+with open(f'README.md') as readme_file:
+    readme_str = readme_file.read()
+    modified_readme = re.sub(pattern, replacement.format(repo_name=repo_name, branch_name=branch_name), readme_str)
+
+with open(f'README.md', 'w') as readme_file:
+    readme_file.write(modified_readme)
