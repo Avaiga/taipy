4e40551bbc899957d11afcb11fc4eebae285cec2
Toan Quach <93168955+toan-quach@users.noreply.github.com>
2023-09-07 15:00:18 +0700

    Update src/taipy/core/data/data_node.py
    Co-authored-by: Đỗ Trường Giang <do.giang@avaiga.com>

--- src/taipy/core/data/data_node.py
+++ src/taipy/core/data/data_node.py
@@ -359,7 +359,7 @@ class DataNode(_Entity, _Labeled):
         join operator (_AND_ or _OR_).
 
         Parameters:
-            operators (Union[List[Tuple], Tuple]): 3-tuples or list of 3-tuples (key, value, `Operator^`).
+            operators (Union[List[Tuple], Tuple]): A 3-element tuple or a list of 3-element tuples, each is in the form of (key, value, `Operator^`).
             join_operator (JoinOperator^): The operator used to join the multiple filter
                 3-tuples.
         """
