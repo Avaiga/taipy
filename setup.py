27163a961f12c8848d25279ad0f98dec646243dc
trgiangdo <dtr.giang.1299@gmail.com>
2023-09-11 16:49:43 +0700

    fix: update flask version to > 2.3.3 on setup.py

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         "Programming Language :: Python :: 3.11",
     ],
     install_requires=[
-        "flask>=2.2,<2.3",
+        "flask>=2.3.3,<2.4",
         "flask-restful>=0.3.9,<0.4",
         "flask-migrate>=3.1,<4.0",
         "flask-jwt-extended>=4.3,<5.0",
