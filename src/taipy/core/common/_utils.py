28ea26a1fa85d2c55d8c517859deb5ac4391bf2f
Toan Quach <shiro@Shiros-MacBook-Pro.local>
2023-09-14 09:30:13 +0700

    improve path for version file

--- src/taipy/core/common/_utils.py
+++ src/taipy/core/common/_utils.py
@@ -85,7 +85,6 @@ def _fcts_to_dict(objs):
 
 def _init_version():
     with open(f"{Path(os.path.abspath(__file__)).parent.parent}{os.sep}version.json") as version_file:
-        # with open(f"{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}{os.sep}version.json") as version_file:
         version = json.load(version_file)
         version_string = f'{version.get("major", 0)}.{version.get("minor", 0)}.{version.get("patch", 0)}'
         if vext := version.get("ext"):
