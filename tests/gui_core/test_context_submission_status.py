9f41de0581d1c020676acdaca54c1276edbb3a5c
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2023-09-21 17:49:53 +0200

    format

--- tests/gui_core/test_context_submission_status.py
+++ tests/gui_core/test_context_submission_status.py
@@ -216,8 +216,7 @@ class TestGuiCoreContext_SubmissionStatus:
             assert status == expected_status
 
     def test_no_job(self):
-        with patch('src.taipy.gui_core._context.core_get', side_effect=mock_core_get):
+        with patch("src.taipy.gui_core._context.core_get", side_effect=mock_core_get):
             gui_core_context = _GuiCoreContext(Mock())
             status = gui_core_context._get_submittable_status([])
             assert status == _SubmissionStatus.UNDEFINED
-
