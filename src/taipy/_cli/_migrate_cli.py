61622960d454bcb57201fe8278a51657723ec601
Joao Andre <joao.dasilva@avaiga.com>
2023-10-01 21:51:41 -0300

    feat: add missing repositories

--- src/taipy/_cli/_migrate_cli.py
+++ src/taipy/_cli/_migrate_cli.py
@@ -8,12 +8,11 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
-
+import argparse
 import sys
 
 from taipy._cli._base_cli import _CLI
-
-from .migrate import _migrate_fs_entities
+from taipy._cli._migrate import _migrate_fs_entities, _migrate_mongo_entities, _migrate_sql_entities
 
 
 class _MigrateCLI:
@@ -22,14 +21,23 @@ class _MigrateCLI:
         migrate_parser = _CLI._add_subparser(
             "migrate", help="Migrate entities from old taipy versions to current taipy version."
         )
-        migrate_parser.add_argument("--path", help="The path to the File System Repository folder")
+        migrate_parser.add_argument(
+            "--repository-type", nargs="*", help="The path to the File System Repository folder"
+        )
 
     @classmethod
     def parse_arguments(cls):
         args = _CLI._parse()
 
         if getattr(args, "which", None) == "migrate":
-            path = sys.argv[-1]  # last argument is path to FS folder
-            _migrate_fs_entities(path)
+            repository_type = args.repository_type[0]
 
+            if repository_type == "filesystem":
+                _migrate_fs_entities(args.repository_type[1])
+            elif repository_type == "sql":
+                _migrate_sql_entities(args.repository_type[1])
+            elif repository_type == "mongo":
+                _migrate_mongo_entities(*args.repository_type[1:])
+            else:
+                raise argparse.ArgumentError(f"Unknown repository type {repository_type}")
             sys.exit(0)
