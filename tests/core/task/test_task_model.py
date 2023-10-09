b68607b9719b724ea537e15e9d6bb27c97ab9cac
jrobinAV <88036007+jrobinAV@users.noreply.github.com>
2023-10-09 11:11:09 +0200

    Update test to match removal of deprecated fields

--- tests/core/task/test_task_model.py
+++ tests/core/task/test_task_model.py
@@ -15,41 +15,6 @@ from src.taipy.core.task._task_model import _TaskModel
 from taipy.config.common.scope import Scope
 
 
-def test_deprecated_parent_id():
-    model = _TaskModel.from_dict(
-        {
-            "id": "id",
-            "config_id": "config_id",
-            "parent_id": "owner_id",
-            "parent_ids": ["parent_id"],
-            "input_ids": ["input_id"],
-            "function_name": "function_name",
-            "function_module": "function_module",
-            "output_ids": ["output_id"],
-            "version": "latest",
-        }
-    )
-    assert model.owner_id == "owner_id"
-
-
-def test_override_deprecated_parent_id():
-    model = _TaskModel.from_dict(
-        {
-            "id": "id",
-            "config_id": "config_id",
-            "parent_id": "parent_id",
-            "owner_id": "owner_id",
-            "parent_ids": ["parent_id"],
-            "input_ids": ["input_id"],
-            "function_name": "function_name",
-            "function_module": "function_module",
-            "output_ids": ["output_id"],
-            "version": "latest",
-        }
-    )
-    assert model.owner_id == "owner_id"
-
-
 def test_none_properties_attribute_compatible():
     model = _TaskModel.from_dict(
         {
@@ -62,6 +27,7 @@ def test_none_properties_attribute_compatible():
             "function_module": "function_module",
             "output_ids": ["output_id"],
             "version": "latest",
+            "skippable": False,
         }
     )
     assert len(model.properties) == 0
@@ -79,6 +45,7 @@ def test_skippable_compatibility_with_non_existing_output():
             "function_module": "function_module",
             "output_ids": ["output_id"],
             "version": "latest",
+            "skippable": False,
         }
     )
     assert not model.skippable
@@ -96,53 +63,16 @@ def test_skippable_compatibility_with_no_output():
             "function_module": "function_module",
             "output_ids": [],
             "version": "latest",
+            "skippable": False,
         }
     )
     assert not model.skippable
 
 
-def test_skippable_compatibility_with_one_cacheable_output():
-    manager = _DataManagerFactory._build_manager()
-    manager._set(InMemoryDataNode("cfg_id", Scope.SCENARIO, id="dn_id", properties={"cacheable": True}))
-
-    model = _TaskModel.from_dict(
-        {
-            "id": "id",
-            "config_id": "config_id",
-            "owner_id": "owner_id",
-            "parent_ids": ["parent_id"],
-            "input_ids": ["input_id"],
-            "function_name": "function_name",
-            "function_module": "function_module",
-            "output_ids": ["dn_id"],
-            "version": "latest",
-        }
-    )
-    assert model.skippable
-
-
-def test_skippable_compatibility_with_one_non_cacheable_output():
-    manager = _DataManagerFactory._build_manager()
-    manager._set(InMemoryDataNode("cfg_id", Scope.SCENARIO, id="dn_id", properties={"cacheable": False}))
-    model = _TaskModel.from_dict(
-        {
-            "id": "id",
-            "config_id": "config_id",
-            "owner_id": "owner_id",
-            "parent_ids": ["parent_id"],
-            "input_ids": ["input_id"],
-            "function_name": "function_name",
-            "function_module": "function_module",
-            "output_ids": ["dn_id"],
-            "version": "latest",
-        }
-    )
-    assert not model.skippable
-
-
-def test_skippable_compatibility_with_one_non_cacheable_existing_output():
+def test_skippable_compatibility_with_one_output():
     manager = _DataManagerFactory._build_manager()
     manager._set(InMemoryDataNode("cfg_id", Scope.SCENARIO, id="dn_id"))
+
     model = _TaskModel.from_dict(
         {
             "id": "id",
@@ -154,57 +84,16 @@ def test_skippable_compatibility_with_one_non_cacheable_existing_output():
             "function_module": "function_module",
             "output_ids": ["dn_id"],
             "version": "latest",
-        }
-    )
-    assert not model.skippable
-
-
-def test_skippable_compatibility_with_all_cacheable_outputs():
-    manager = _DataManagerFactory._build_manager()
-    manager._set(InMemoryDataNode("cfg_id", Scope.SCENARIO, id="dn_id", properties={"cacheable": True}))
-    manager._set(InMemoryDataNode("cfg_id_2", Scope.SCENARIO, id="dn_2_id", properties={"cacheable": True}))
-    model = _TaskModel.from_dict(
-        {
-            "id": "id",
-            "config_id": "config_id",
-            "owner_id": "owner_id",
-            "parent_ids": ["parent_id"],
-            "input_ids": ["input_id"],
-            "function_name": "function_name",
-            "function_module": "function_module",
-            "output_ids": ["dn_id", "dn_2_id"],
-            "version": "latest",
+            "skippable": True,
         }
     )
     assert model.skippable
 
 
-def test_skippable_compatibility_with_one_cacheable_output_over_two():
+def test_skippable_compatibility_with_many_outputs():
     manager = _DataManagerFactory._build_manager()
-    manager._set(InMemoryDataNode("cfg_id", Scope.SCENARIO, id="dn_id", properties={"cacheable": True}))
-    manager._set(InMemoryDataNode("cfg_id_2", Scope.SCENARIO, id="dn_2_id", properties={"cacheable": False}))
-
-    model = _TaskModel.from_dict(
-        {
-            "id": "id",
-            "config_id": "config_id",
-            "owner_id": "owner_id",
-            "parent_ids": ["parent_id"],
-            "input_ids": ["input_id"],
-            "function_name": "function_name",
-            "function_module": "function_module",
-            "output_ids": ["dn_id", "dn_2_id"],
-            "version": "latest",
-        }
-    )
-    assert not model.skippable
-
-
-def test_skippable_compatibility_with_one_cacheable_output_and_one_non_cacheable_existing():
-    manager = _DataManagerFactory._build_manager()
-    manager._set(InMemoryDataNode("cfg_id", Scope.SCENARIO, id="dn_id", properties={"cacheable": True}))
+    manager._set(InMemoryDataNode("cfg_id", Scope.SCENARIO, id="dn_id"))
     manager._set(InMemoryDataNode("cfg_id_2", Scope.SCENARIO, id="dn_2_id"))
-
     model = _TaskModel.from_dict(
         {
             "id": "id",
@@ -216,6 +105,7 @@ def test_skippable_compatibility_with_one_cacheable_output_and_one_non_cacheable
             "function_module": "function_module",
             "output_ids": ["dn_id", "dn_2_id"],
             "version": "latest",
+            "skippable": True,
         }
     )
-    assert not model.skippable
+    assert model.skippable
