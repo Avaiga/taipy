626ef42c6bc584609134109331915e33c2f59f03
Toan Quach <shiro@Shiros-MBP.STUSGS.RMIT.EDU.VN>
2023-11-11 13:51:22 +0700

    remove breakpoint

--- tests/core/submission/test_submission_manager_with_sql_repo.py
+++ tests/core/submission/test_submission_manager_with_sql_repo.py
@@ -86,7 +86,6 @@ def test_get_all_submission(init_sql_repo):
             submission_manager._set(
                 Submission("entity_id", f"submission_{version_name}_{i}", version=f"{version_name}")
             )
-    breakpoint()
     assert len(submission_manager._get_all()) == 1
 
     version_manager._set_experiment_version("xyz")
