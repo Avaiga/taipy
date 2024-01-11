b55959d0fd694e0dfcc4c7921dfb11a5f3aa06db
jrobinAV <jeanrobin.medori@avaiga.com>
2024-01-11 17:37:02 +0100

    Clean all

--- gui/src/icons/index.ts
+++
@@ -1,8 +0,0 @@
-export { Cycle } from "./cycle";
-export { Datanode } from "./datanode";
-export { Job } from "./job";
-export { Sequence } from "./sequence";
-export { Scenario } from "./scenario";
-export { Task } from "./task";
-export { Input } from "./input";
-export { Output } from "./output";
--- gui/src/index.ts
+++
@@ -1,8 +0,0 @@
-import ScenarioSelector from "./ScenarioSelector";
-import ScenarioViewer from "./ScenarioViewer";
-import ScenarioDag from "./ScenarioDag";
-import NodeSelector from "./NodeSelector";
-import JobSelector from "./JobSelector";
-import DataNodeViewer from "./DataNodeViewer";
-
-export { ScenarioSelector, ScenarioDag, ScenarioViewer as Scenario, NodeSelector as DataNodeSelector, JobSelector, DataNodeViewer as DataNode };
