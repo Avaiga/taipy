ea9d22e2b5330b0d3d2ae7b2086e490cf94cde68
Vuillemot Florian <florian.vuillemot@avaiga.com>
2024-01-16 19:03:53 +0100

    update python

--- tools/packages/check-dependencies.sh
+++ tools/packages/check-dependencies.sh
@@ -1,15 +1,15 @@
 PACKAGES="taipy-core/setup.requirements.txt taipy/setup.requirements.txt taipy-gui/setup.requirements.txt taipy-config/setup.requirements.txt taipy-rest/setup.requirements.txt"
 
 # Ensure package are using the same version of dependencies
-python3 check-dependencies.py ensure-same-version $PACKAGES
+python check-dependencies.py ensure-same-version $PACKAGES
 
 # Retrieve requirements then install them to check the latest requirements installable
 cat $PACKAGES | grep -v "taipy" | grep -Eo "[^>]*" | grep -v "=" > requirements.txt.tmp
-python3 -m venv tmp-venv
-./tmp-venv/bin/python3 -m pip install -r requirements.txt.tmp
-./tmp-venv/bin/python3 -m pip freeze > real-requirements.txt
+python -m venv tmp-venv
+./tmp-venv/bin/python -m pip install -r requirements.txt.tmp
+./tmp-venv/bin/python -m pip freeze > real-requirements.txt
 grep -Ff requirements.txt.tmp real-requirements.txt
 rm -rf tmp-venv requirements.txt.tmp
 
 # Update requirements based on the latest installable requirements
-python3 check-dependencies.py dependencies-to-update real-requirements.txt $PACKAGES
+python check-dependencies.py dependencies-to-update real-requirements.txt $PACKAGES
