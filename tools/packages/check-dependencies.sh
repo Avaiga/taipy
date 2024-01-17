ae6e7f2d54d0d0cc0ef58cd8a2fcbc11f6c4f6d3
Vuillemot Florian <florian.vuillemot@avaiga.com>
2024-01-17 10:13:31 +0100

    update dep

--- tools/packages/check-dependencies.sh
+++ tools/packages/check-dependencies.sh
@@ -5,7 +5,7 @@ python check-dependencies.py raw-packages $PACKAGES > requirements.txt.tmp
 python -m venv tmp-venv > /dev/null
 ./tmp-venv/bin/python3 -m pip install -r requirements.txt.tmp > /dev/null
 ./tmp-venv/bin/python3 -m pip freeze > real-requirements.txt
-grep -Ff requirements.txt.tmp real-requirements.txt > new-requirements.txt
+grep -Ff real-requirements.txt requirements.txt.tmp > new-requirements.txt
 rm -rf tmp-venv requirements.txt.tmp real-requirements.txt
 
 # Update requirements based on the latest installable requirements
