9a2379f896926d692d88d2b17609bf98ad420823
Joao Andre <joao.dasilva@avaiga.com>
2024-01-16 21:41:53 -0300

    chore: add python 3.12 on testing workflows

--- tools/packages/taipy-rest/setup.py
+++ tools/packages/taipy-rest/setup.py
@@ -48,6 +48,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     install_requires=requirements,
 )
