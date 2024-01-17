3c440b8082d904cc4c660d9f1cebbd6c1b55e7cf
Vuillemot Florian <florian.vuillemot@avaiga.com>
2024-01-17 16:14:18 +0100

    update sh

--- tools/packages/check-dependencies.sh
+++ tools/packages/check-dependencies.sh
@@ -1,10 +1,10 @@
-PACKAGES=taipy-*/*requirements.txt
+PACKAGES=taipy*/*requirements.txt
 
 # Retrieve requirements then install them to check the latest requirements installable
 python check-dependencies.py raw-packages $PACKAGES > requirements.txt.tmp
 python -m venv tmp-venv > /dev/null
 ./tmp-venv/bin/python3 -m pip install -r requirements.txt.tmp > /dev/null
 ./tmp-venv/bin/python3 -m pip freeze > real-requirements.txt
-
+cat real-requirements.txt
 # Update requirements based on the latest installable requirements
 python check-dependencies.py dependencies-to-update real-requirements.txt $PACKAGES
