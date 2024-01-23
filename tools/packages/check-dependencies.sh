715bf911dcbb7fb3120cb87ca7d5bf86d77bfb0c
Vuillemot Florian <florian.vuillemot@avaiga.com>
2024-01-23 10:41:36 +0100

    Debug

--- tools/packages/check-dependencies.sh
+++ tools/packages/check-dependencies.sh
@@ -11,6 +11,7 @@ python check-dependencies.py generate-raw-requirements > raw-requirements.txt
 python -m venv tmp-venv > /dev/null
 ./tmp-venv/bin/python3 -m pip install -r raw-requirements.txt > /dev/null
 ./tmp-venv/bin/python3 -m pip freeze > new-requirements.txt
+cat new-requirements.txt
 
 # Display dependencies summary.
 python check-dependencies.py dependencies-summary new-requirements.txt
