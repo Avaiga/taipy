e6e27e8ac7e607ce805bc6afbca2b8c4157681f9
FORCHA PEARL <forchapearl1@gmail.com>
2024-01-28 00:47:03 +0100

    added get_cycles_by_overlapping_date

--- taipy/core/cycle/_cycle_manager.py
+++ taipy/core/cycle/_cycle_manager.py
@@ -152,12 +152,8 @@ class _CycleManager(_Manager[Cycle]):
         return [cycle for cycle in cycles if cdt(cycle)]
 
     @classmethod
-    def _get_scenarios_by_time(cls , at: datetime):
-        from ..scenario._scenario_manager_factory import _ScenarioManagerFactory
-
-        scenarios = []
-        for scenario in _ScenarioManagerFactory._build_manager()._get_all():
-            cycle = scenario.cycle
-            if cycle.start_date <= at and at <=  cycle.end_date:
-                scenarios.append(scenario)
-        return scenarios
+    def _get_cycles_by_overlapping_date(cls , date: datetime) -> List[Cycle]:
+        cycles=cls._get_all()
+        return cls._get_cycles_cdt(
+            lambda cycle: cycle.start_date <= date and date <= cycle.end_date, cycles
+        )
