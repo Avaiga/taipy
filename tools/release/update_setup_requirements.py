ec40019eeeda2c7cfe01720a0759e3e2d581b28b
Joao Andre <joao.dasilva@avaiga.com>
2024-02-15 22:50:45 -0300

    feat: add optional publish step to build and release

--- tools/release/update_setup_requirements.py
+++ tools/release/update_setup_requirements.py
@@ -5,17 +5,20 @@ from typing import Dict
 BASE_PATH = "./tools/packages"
 
 
-def __build_taipy_package_line(line: str, version: str) -> str:
-    return f"{line.strip()} @ https://github.com/Avaiga/taipy/releases/download/{version}/{version}.tar.gz\n"
+def __build_taipy_package_line(line: str, version: str, publish_on_py_pi: bool) -> str:
+    _line = line.strip()
+    if publish_on_py_pi:
+        return f"{_line}=={version}"
+    return f"{_line} @ https://github.com/Avaiga/taipy/releases/download/{version}/{version}.tar.gz\n"
 
 
-def update_setup_requirements(package: str, versions: Dict) -> None:
+def update_setup_requirements(package: str, versions: Dict, publish_on_py_pi: bool) -> None:
     _path = os.path.join(BASE_PATH, package, "setup.requirements.txt")
     lines = []
     with open(_path, mode="r") as req:
         for line in req:
             if v := versions.get(line.strip()):
-                line = __build_taipy_package_line(line, v)
+                line = __build_taipy_package_line(line, v, publish_on_py_pi)
             lines.append(line)
 
     with open(_path, 'w') as file:
@@ -31,5 +34,6 @@ if __name__ == "__main__":
         "taipy-rest": sys.argv[5],
         "taipy-templates": sys.argv[6],
     }
+    _publish_on_py_pi = bool(sys.argv[7])
 
-    update_setup_requirements(_package, _versions)
+    update_setup_requirements(_package, _versions, _publish_on_py_pi)
