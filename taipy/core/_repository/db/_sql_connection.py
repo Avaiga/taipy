1e697270799d5f45bc002dd2281e26cdf1e52f1f
Toan Quach <shiro@Shiros-MacBook-Pro.local>
2024-02-27 14:18:39 +0700

    lower timeout

--- taipy/core/_repository/db/_sql_connection.py
+++ taipy/core/_repository/db/_sql_connection.py
@@ -77,4 +77,4 @@ def _build_connection() -> Connection:
 
 @lru_cache
 def __build_connection(db_location: str):
-    return sqlite3.connect(db_location, check_same_thread=False, timeout=200)
+    return sqlite3.connect(db_location, check_same_thread=False, timeout=15)
