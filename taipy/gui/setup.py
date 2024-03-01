750afd0337dad810e7ce3c87202dc813129be0da
Dinh Long Nguyen <dinhlongviolin1@gmail.com>
2024-03-01 21:36:26 +0700

    Gui: Use the correct webapp Path (#914)

--- taipy/gui/setup.py
+++ taipy/gui/setup.py
@@ -56,7 +56,7 @@ extras_require = {
 
 
 def _build_webapp():
-    already_exists = Path("./webapp/index.html").exists()
+    already_exists = Path("./taipy/gui/webapp/index.html").exists()
     if not already_exists:
         os.system("cd ../../frontend/taipy-gui/dom && npm ci")
         os.system("cd ../../frontend/taipy-gui && npm ci --omit=optional && npm run build")
