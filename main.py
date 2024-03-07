789f9646799e609c2a9581a0535f360cc6f4ad5f
SusheelThapa <susheelthapa90@gmail.com>
2024-03-07 23:53:21 +0545

    setup layout for nepal page

--- main.py
+++ main.py
@@ -1,13 +1,51 @@
 from taipy.gui import Gui
 
+from utils.graphs import bubble_chart_whole
+
 from pages.root import root_md
 from pages.district.district import district_md
 
+nepal = """
+# **Nepal**{: .color-primary} Statistics
+
+<br/>
+
+<|layout|columns= 1 1 1 1|gap=100px|class_name=m1|
+
+<|card|
+**Total Population**{:.color-primary}
+<|{total_population}|text|class_name=h2|>
+|>
+
+<|card|
+**Total Male Population**{:.color-primary}
+<|{total_male_population}|text|class_name=h2|>
+|>
+
+<|card|
+**Total Female Population**{:.color-primary}
+<|{total_female_population}|text|class_name=h2|>
+|>
+
+|>
+
+<br/>
+
+# Visualization in **Graphs**{:.color-primary}
+
+<|layout|columns=1 1|class_name=m2|
+
+
+|>
+"""
+
+total_population, total_male_population, total_female_population = (
+    1000, 1000, 1000)
 
 pages = {
     "/": root_md,
     "district": district_md,
-    "nepal": "Nepal",
+    "nepal": nepal,
     "map": "Map"
 }
 
