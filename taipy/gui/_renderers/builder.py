9c7e35fe628f5be883b6a172ca1bff556df9632e
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2024-03-12 09:18:07 +0100

    no error when the column desc is undefined (#958)
    * no error when the column desc is undefined
    add a message to let the user know that he needs to bind data variables
    resolves #466
    
    * Fab's comments
    
    ---------
    
    Co-authored-by: Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
    (cherry picked from commit 60017012eefd72bc667c5bea6dd823bdee1d86cd)

--- taipy/gui/_renderers/builder.py
+++ taipy/gui/_renderers/builder.py
@@ -775,6 +775,8 @@ class _Builder:
                 else:
                     self.__set_default_value(var_name, var_type=var_type)
         else:
+            if var_type == PropertyType.data:
+                _warn(f"{self.__control_type}.data property should be bound.")
             value = self.__attributes.get(var_name)
             if value is not None:
                 if native_type:
