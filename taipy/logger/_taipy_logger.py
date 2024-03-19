9720572cb5a0d5c307cc3d52122b8d9d91275dbc
jrobinAV <jeanrobin.medori@avaiga.com>
2024-03-19 10:43:20 +0100

    add msecs to default log format

--- taipy/logger/_taipy_logger.py
+++ taipy/logger/_taipy_logger.py
@@ -34,7 +34,7 @@ class _TaipyLogger:
             cls.__logger.setLevel(logging.INFO)
             ch = logging.StreamHandler(sys.stdout)
             ch.setLevel(logging.INFO)
-            formatter = logging.Formatter("[%(asctime)s][%(name)s][%(levelname)s] %(message)s", "%Y-%m-%d %H:%M:%S")
-            ch.setFormatter(formatter)
+            frmter = logging.Formatter("[%(asctime)s.%03d][%(name)s][%(levelname)s] %(message)s", "%Y-%m-%d %H:%M:%S")
+            ch.setFormatter(frmter)
             cls.__logger.addHandler(ch)
         return cls.__logger
