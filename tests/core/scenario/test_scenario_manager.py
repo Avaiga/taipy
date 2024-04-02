01a5579209325526b2033f3de7d7667e93594712
Luke-0162 <70721200+Luke-0162@users.noreply.github.com>
2024-04-02 15:48:58 +0200

    Update tests/core/scenario/test_scenario_manager.py
    Co-authored-by: Jean-Robin <jeanrobin.medori@avaiga.com>

--- tests/core/scenario/test_scenario_manager.py
+++ tests/core/scenario/test_scenario_manager.py
@@ -817,6 +817,7 @@ def test_get_primary_scenarios_sorted():
     scenario_1_cfg = Config.configure_scenario(id="scenario_1", frequency=Frequency.DAILY)
     scenario_2_cfg = Config.configure_scenario(id="scenario_2", frequency=Frequency.DAILY)
 
+not_primary_scenario = _ScenarioManager._create(scenario_1_cfg, name="not_primary_scenario")
     scenario_1 = _ScenarioManager._create(scenario_1_cfg, name="B_scenario")
     scenario_2 = _ScenarioManager._create(scenario_2_cfg, name="A_scenario", creation_date=datetime.now() + timedelta(days=2))
     scenario_3 = _ScenarioManager._create(scenario_2_cfg, name="C_scenario", creation_date=datetime.now() + timedelta(days=4))
