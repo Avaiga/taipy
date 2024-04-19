a3f128486a0c979b303150ea3399aa63305a58d7
trgiangdo <dtr.giang.1299@gmail.com>
2024-04-19 09:41:35 +0700

    fix: remove unuse import

--- taipy/core/data/_data_manager.py
+++ taipy/core/data/_data_manager.py
@@ -26,7 +26,6 @@ from ..exceptions.exceptions import InvalidDataNodeType
 from ..notification import Event, EventEntityType, EventOperation, Notifier, _make_event
 from ..scenario.scenario_id import ScenarioId
 from ..sequence.sequence_id import SequenceId
-from ._abstract_file import _FileDataNodeMixin
 from ._data_fs_repository import _DataFSRepository
 from ._file_datanode_mixin import _FileDataNodeMixin
 from .data_node import DataNode
