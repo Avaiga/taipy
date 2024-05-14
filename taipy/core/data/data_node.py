c59176d36102875d409adadaac509ad34e3aca11
jrobinAV <jeanrobin.medori@avaiga.com>
2024-05-14 17:28:29 +0200

    linter

--- taipy/core/data/data_node.py
+++ taipy/core/data/data_node.py
@@ -89,13 +89,12 @@ class DataNode(_Entity, _Labeled):
         parent_ids (Optional[Set[str]]): The set of identifiers of the parent tasks.
         last_edit_date (datetime): The date and time of the last modification.
         edits (List[Edit^]): The list of Edits (an alias for dict) containing metadata about each
-            data edition including but not limited to timestamp, comments, job_id:
+            data edition including but not limited to:
 
                 - timestamp: The time instant of the writing
                 - comments: Representation of a free text to explain or comment on a data change
                 - job_id: Only populated when the data node is written by a task execution and
                     corresponds to the job's id.
-                    
             Additional metadata related to the edition made to the data node can also be provided in Edits.
         version (str): The string indicates the application version of the data node to
             instantiate. If not provided, the current version is used.
