e126f4db09171638aaf54f231064d5e3d8496761
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2024-06-04 09:13:08 +0200

    Fab's comments

--- frontend/taipy-gui/src/components/Taipy/Chart.tsx
+++ frontend/taipy-gui/src/components/Taipy/Chart.tsx
@@ -218,7 +218,6 @@ const TaipyPlotlyButtons: ModeBarButtonAny[] = [
             path: "M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z",
         },
         click: function (gd: HTMLElement, evt: Event) {
-            const fs = gd.classList.contains("full-screen");
             const div = gd.querySelector("div.svg-container") as HTMLDivElement;
             if (!div) {
                 return;
@@ -227,11 +226,9 @@ const TaipyPlotlyButtons: ModeBarButtonAny[] = [
             if (!height) {
                 gd.setAttribute("data-height", getComputedStyle(div).height);
             }
-            (evt.currentTarget as HTMLElement).setAttribute(
-                "data-title",
-                gd.classList.toggle("full-screen") ? "Exit Full screen" : "Full screen"
-            );
-            if (height && fs) {
+            const fs = gd.classList.toggle("full-screen");
+            (evt.currentTarget as HTMLElement).setAttribute("data-title", fs ? "Exit Full screen" : "Full screen");
+            if (height && !fs) {
                 div.attributeStyleMap.set("height", height);
             }
             window.dispatchEvent(new Event("resize"));
