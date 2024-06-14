86740ea5b9f89b7da36f51ebcf18be2acbae6804
dinhlongviolin1 <dinhlongviolin1@gmail.com>
2024-06-14 11:20:35 +0700

    dont export main app -> reduce bundle size

--- frontend/taipy-gui/base/src/exports.ts
+++ frontend/taipy-gui/base/src/exports.ts
@@ -1,5 +1,4 @@
-import { TaipyApp } from "./app";
 import { WsAdapter } from "./wsAdapter";
 import { sendWsMessage } from "../../src/context/wsUtils";
 
-export { TaipyApp, WsAdapter, sendWsMessage }
+export { WsAdapter, sendWsMessage }
