2d73d090a5432a2ff2ebbd94d72d39e390c9d5d8
satoshi-sh <satoss1108@gmail.com>
2024-07-17 07:03:17 -0500

    Add testings for the delta colors

--- frontend/taipy-gui/src/components/Taipy/Metric.spec.tsx
+++ frontend/taipy-gui/src/components/Taipy/Metric.spec.tsx
@@ -238,6 +238,16 @@ describe("Metric Component", () => {
         });
     });
 
+    it("applies style correctly when positiveDeltaColor is set", async () => {
+        render(<Metric delta={10} positiveDeltaColor="#FF4136" testId="test-id" />);
+        await waitFor(() => {
+            const elt = document.querySelector(".delta");
+            expect(elt).toHaveStyle({
+                    fill: "rgb(255, 65, 54)"
+                });
+        });
+    });
+
     it("processes type and threshold props correctly when type is linear", async () => {
         render(<Metric type="linear" threshold={50} testId="test-id" />);
         await waitFor(() => {
