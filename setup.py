90f28a397d4fd5cf463f2aa57f885ff968024ee5
Joao Andre <joao.dasilva@avaiga.com>
2024-08-15 22:14:54 -0300

    feat: add back setup.py to enable custom package builds

--- setup.py
+++ setup.py
@@ -9,30 +9,31 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-"""The setup script for taipy package"""
-
+import os
 import json
 import platform
-from pathlib import Path
 import subprocess
-
-from setuptools import find_packages, setup
+from pathlib import Path
+from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py
 
 root_folder = Path(__file__).parent
 
-package_desc = (root_folder / "package_desc.md").read_text("UTF-8")
-
-with open(root_folder / "taipy" / "version.json") as version_file:
+# get current version
+with open(os.path.join("taipy", "version.json")) as version_file:
     version = json.load(version_file)
     version_string = f'{version.get("major", 0)}.{version.get("minor", 0)}.{version.get("patch", 0)}'
     if vext := version.get("ext"):
         version_string = f"{version_string}.{vext}"
 
-requirements = [r for r in (root_folder / "setup.requirements.txt").read_text("UTF-8").splitlines() if r]
+def get_requirements():
+    reqs = set()
+    for pkg in (root_folder / "tools" / "packages").iterdir():
+        requirements_file = pkg / "setup.requirements.txt"
+        if requirements_file.exists():
+            reqs.update(requirements_file.read_text("UTF-8").splitlines())
 
-test_requirements = ["pytest>=3.8"]
-
+    return [r for r in reqs if r and not r.startswith("taipy")]
 
 class NPMInstall(build_py):
     def run(self):
@@ -44,10 +45,9 @@ class NPMInstall(build_py):
         )
         build_py.run(self)
 
-
 setup(
     version=version_string,
-    install_requires=requirements,
+    install_requires=get_requirements(),
     packages=find_packages(include=["taipy", "taipy.*"]),
     extras_require={
         "ngrok": ["pyngrok>=5.1,<6.0"],
