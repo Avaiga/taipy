295010671ef15c265f2e0735372bc775ccf65259
Fred Lefévère-Laoide <90181748+FredLL-Avaiga@users.noreply.github.com>
2024-09-18 10:41:23 +0200

    Chat sender avatar (#1800)
    * Chat sender avatar
    show_sender default to False
    resolves #1798
    
    * test
    
    ---------
    
    Co-authored-by: Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>

--- taipy/gui/_renderers/factory.py
+++ taipy/gui/_renderers/factory.py
@@ -98,7 +98,7 @@ class _Factory:
                 ("sender_id",),
                 ("height",),
                 ("page_size", PropertyType.number, 50),
-                ("show_sender", PropertyType.boolean, True),
+                ("show_sender", PropertyType.boolean, False),
             ]
         ),
         "chart": lambda gui, control_type, attrs: _Builder(
