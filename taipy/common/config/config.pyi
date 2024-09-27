c3c0add20804a6eb445b7f5135702640d84146a8
joaoandre-avaiga <joaoandre-avaiga@users.noreply.github.com>
2024-09-27 01:25:49 +0000

    Update config.pyi

--- taipy/common/config/config.pyi
+++ taipy/common/config/config.pyi
@@ -8,6 +8,21 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
+import json
+from datetime import timedelta
+from typing import Any, Callable, Dict, List, Optional, Union
+
+from taipy.common.config._config import _Config
+from taipy.core.config import CoreSection, DataNodeConfig, JobConfig, ScenarioConfig, TaskConfig
+
+from .checker.issue_collector import IssueCollector
+from .common._classproperty import _Classproperty
+from .common._config_blocker import _ConfigBlocker
+from .common.frequency import Frequency
+from .common.scope import Scope
+from .global_app.global_app_config import GlobalAppConfig
+from .section import Section
+from .unique_section import UniqueSection
 
 class Config:
     """Singleton class that manages the configuration of a Taipy application.
