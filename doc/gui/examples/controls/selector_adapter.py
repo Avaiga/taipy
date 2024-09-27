4e9868e411966ebb9c4ac4bd6c440d9e36e8effe
Fred Lefévère-Laoide <90181748+FredLL-Avaiga@users.noreply.github.com>
2024-09-27 09:13:31 +0200

    selector example (#1843)
    * selector example
    
    * Fab's comments
    
    * Fab's comment
    
    Co-authored-by: Fabien Lelaquais <86590727+FabienLelaquais@users.noreply.github.com>
    
    * Fab's comment
    
    Co-authored-by: Fabien Lelaquais <86590727+FabienLelaquais@users.noreply.github.com>
    
    * Fab's comment
    
    Co-authored-by: Fabien Lelaquais <86590727+FabienLelaquais@users.noreply.github.com>
    
    * Fab's comment
    
    Co-authored-by: Fabien Lelaquais <86590727+FabienLelaquais@users.noreply.github.com>
    
    * Fab's comment
    
    Co-authored-by: Fabien Lelaquais <86590727+FabienLelaquais@users.noreply.github.com>
    
    * Fab's comment
    
    Co-authored-by: Fabien Lelaquais <86590727+FabienLelaquais@users.noreply.github.com>
    
    * Fab's comment
    
    Co-authored-by: Fabien Lelaquais <86590727+FabienLelaquais@users.noreply.github.com>
    
    * Fab's comment
    
    Co-authored-by: Fabien Lelaquais <86590727+FabienLelaquais@users.noreply.github.com>
    
    * Fab's comment
    
    Co-authored-by: Fabien Lelaquais <86590727+FabienLelaquais@users.noreply.github.com>
    
    * Fab's comment
    
    Co-authored-by: Fabien Lelaquais <86590727+FabienLelaquais@users.noreply.github.com>
    
    * Fab's comments
    
    ---------
    
    Co-authored-by: Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
    Co-authored-by: Fabien Lelaquais <86590727+FabienLelaquais@users.noreply.github.com>

---
+++ doc/gui/examples/controls/selector_adapter.py
@@ -0,0 +1,37 @@
+# Copyright 2021-2024 Avaiga Private Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+# -----------------------------------------------------------------------------------------
+# To execute this script, make sure that the taipy-gui package is installed in your
+# Python environment and run:
+#     python <script>
+# -----------------------------------------------------------------------------------------
+from taipy.gui import Gui
+
+
+class User:
+    def __init__(self, id, name, birth_year):
+        self.id, self.name, self.birth_year = (id, name, birth_year)
+
+users = [
+    User(231, "Johanna", 1987),
+    User(125, "John", 1979),
+    User(4,   "Peter", 1968),
+    User(31,  "Mary", 1974)
+    ]
+
+user_sel = users[2]
+
+page = """
+<|{user_sel}|selector|lov={users}|type=User|adapter={lambda u: (u.id, u.name)}|>
+"""
+
+if __name__ == "__main__":
+    Gui(page).run(title="Selector - Adapter")
