f554e503f5b995cdfec4c938469f4f5c304c889f
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2024-10-01 09:03:44 +0200

    NoneType not in 3.9

--- taipy/gui/_renderers/builder.py
+++ taipy/gui/_renderers/builder.py
@@ -18,7 +18,6 @@ import xml.etree.ElementTree as etree
 from datetime import date, datetime, time
 from enum import Enum
 from inspect import isclass, isfunction
-from types import NoneType
 from urllib.parse import quote
 
 from .._warnings import _warn
@@ -572,7 +571,7 @@ class _Builder:
             self.set_boolean_attribute("compare", True)
             self.__set_string_attribute("on_compare")
 
-        if not isinstance(self.__attributes.get("style"), (NoneType, dict, _MapDict)):
+        if not isinstance(self.__attributes.get("style"), (type(None), dict, _MapDict)):
             _warn("Table: property 'style' has been renamed to 'row_class_name'.")
         if row_class_name := self.__attributes.get("row_class_name"):
             if isfunction(row_class_name):
