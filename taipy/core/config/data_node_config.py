97822d81ca79afb33d01fdc015434f28f7fc0165
adesh_ghadage <adeshghadage9@gmail.com>
2024-10-02 17:59:57 +0530

    linter not defined error solved

--- taipy/core/config/data_node_config.py
+++ taipy/core/config/data_node_config.py
@@ -260,7 +260,9 @@ class DataNodeConfig(Section):
         },
         _STORAGE_TYPE_VALUE_S3_OBJECT: {
             _OPTIONAL_AWS_REGION_PROPERTY: None,
-            _OPTIONAL_AWS_S3_OBJECT_PARAMETERS_PROPERTY: None,
+            _OPTIONAL_AWS_S3_CLIENT_PARAMETERS_PROPERTY: None,
+            _OPTIONAL_AWS_S3_GET_OBJECT_PARAMETERS_PROPERTY: None,
+            _OPTIONAL_AWS_S3_PUT_OBJECT_PARAMETERS_PROPERTY: None,
         },
     }
 
@@ -1097,6 +1099,7 @@ class DataNodeConfig(Section):
             aws_access_key (str): Amazon Web Services ID for to identify account.
             aws_secret_access_key (str): Amazon Web Services access key to authenticate programmatic requests.
             aws_s3_bucket_name (str): The bucket in S3 to read from and to write the data to.
+            aws_s3_object_key (str): The specific S3 object key to configure.
             aws_region (Optional[str]): Self-contained geographic area where Amazon Web Services (AWS)
                 infrastructure is located.
             aws_s3_client_parameters (Optional[dict]): Additional parameters for the S3 client.
@@ -1132,6 +1135,7 @@ class DataNodeConfig(Section):
             properties[cls._OPTIONAL_AWS_S3_GET_OBJECT_PARAMETERS_PROPERTY] = aws_s3_get_object_parameters
         if aws_s3_put_object_parameters is not None:
             properties[cls._OPTIONAL_AWS_S3_PUT_OBJECT_PARAMETERS_PROPERTY] = aws_s3_put_object_parameters
+
         return cls.__configure(id, DataNodeConfig._STORAGE_TYPE_VALUE_S3_OBJECT, scope, validity_period, **properties)
 
     @staticmethod
