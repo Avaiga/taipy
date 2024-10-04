237e11ecf8b7ed44fcc9911593ee4ac964d176bd
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2024-10-04 11:42:29 +0200

    less than in expression resolves #1909

--- taipy/gui/_renderers/_markdown/preproc.py
+++ taipy/gui/_renderers/_markdown/preproc.py
@@ -61,7 +61,7 @@ class _Preprocessor(MdPreprocessor):
     __PROPERTY_RE = re.compile(r"((?:don'?t|not?)\s+)?([a-zA-Z][\.a-zA-Z_$0-9]*(?:\[(?:.*?)\])?)\s*(?:=(.*))?$")
 
     # Error syntax detection regex
-    __MISSING_LEADING_PIPE_RE = re.compile(r"<[^|](.*?)\|>")
+    __MISSING_LEADING_PIPE_RE = re.compile(r"^<[^|](.*?)\|>")
 
     _gui: "Gui"
 
