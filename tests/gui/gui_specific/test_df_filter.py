1fbcc9e8ead003f63be6cd7497e09602c81dd738
dinhlongviolin1 <dinhlongviolin1@gmail.com>
2024-10-05 01:44:52 +0700

    fix test

--- tests/gui/gui_specific/test_df_filter.py
+++ tests/gui/gui_specific/test_df_filter.py
@@ -13,31 +13,30 @@ from taipy.gui.data.decimator.lttb import LTTB
 from taipy.gui.data.decimator.minmax import MinMaxDecimator
 from taipy.gui.data.decimator.rdp import RDP
 from taipy.gui.data.decimator.scatter_decimator import ScatterDecimator
-from taipy.gui.data.utils import _df_data_filter
 
 
 def test_data_filter_1(csvdata):
-    df, _ = _df_data_filter(csvdata[:1500], None, "Daily hospital occupancy", "", MinMaxDecimator(100), {}, False)
+    df, _ = MinMaxDecimator(100)._df_apply_decimator(csvdata[:1500], None, "Daily hospital occupancy", "", {}, False)
     assert df.shape[0] == 100
 
 
 def test_data_filter_2(csvdata):
-    df, _ = _df_data_filter(csvdata[:1500], None, "Daily hospital occupancy", "", LTTB(100), {}, False)
+    df, _ = LTTB(100)._df_apply_decimator(csvdata[:1500], None, "Daily hospital occupancy", "", {}, False)
     assert df.shape[0] == 100
 
 
 def test_data_filter_3(csvdata):
-    df, _ = _df_data_filter(csvdata[:1500], None, "Daily hospital occupancy", "", RDP(n_out=100), {}, False)
+    df, _ = RDP(n_out=100)._df_apply_decimator(csvdata[:1500], None, "Daily hospital occupancy", "", {}, False)
     assert df.shape[0] == 100
 
 
 def test_data_filter_4(csvdata):
-    df, _ = _df_data_filter(csvdata[:1500], None, "Daily hospital occupancy", "", RDP(epsilon=100), {}, False)
+    df, _ = RDP(epsilon=100)._df_apply_decimator(csvdata[:1500], None, "Daily hospital occupancy", "", {}, False)
     assert df.shape[0] == 18
 
 
 def test_data_filter_5(csvdata):
-    df, _ = _df_data_filter(
-        csvdata[:1500], None, "Daily hospital occupancy", "", ScatterDecimator(), {"width": 200, "height": 100}, False
+    df, _ = ScatterDecimator()._df_apply_decimator(
+        csvdata[:1500], None, "Daily hospital occupancy", "", {"width": 200, "height": 100}, False
     )
     assert df.shape[0] == 1150
