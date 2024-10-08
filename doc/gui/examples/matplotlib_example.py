153e5f1d548c7f0322af9bc7c3f2fbae68c6bcd8
SGCODEX <64886313+SGCODEX@users.noreply.github.com>
2024-10-08 11:46:39 +0530

    fixed linter issues

--- doc/gui/examples/matplotlib_example.py
+++ doc/gui/examples/matplotlib_example.py
@@ -1,15 +1,13 @@
 # Integrating Matplotlib into Taipy using figure property
 
 # Import Necessary Libraries:
-from taipy.gui import Gui 
+from taipy.gui import Gui
 import taipy.gui.builder as tgb
-import matplotlib.pyplot as plt  
-from matplotlib.figure import Figure 
-import numpy as np  
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Create a Matplotlib Figure:
-fig = plt.figure(figsize =(5, 4))  
-xx = np.arange(0, 2 * np.pi, 0.01)  
+fig = plt.figure(figsize =(5, 4))
+xx = np.arange(0, 2 * np.pi, 0.01)
 plot = fig.subplots(1,1)
 plot.fill(xx, np.sin(xx), facecolor='none', edgecolor='purple', linewidth=2)
 
