889576dcd8f70f5bfa9747d29980032bdca73a57
SGCODEX <64886313+SGCODEX@users.noreply.github.com>
2024-10-08 15:42:40 +0530

    Updated Code

--- doc/gui/examples/matplotlib_example.py
+++ doc/gui/examples/matplotlib_example.py
@@ -1,15 +1,13 @@
-# Integrating Matplotlib into Taipy using figure property
-
-# Import Necessary Libraries:
-from taipy.gui import Gui
-import taipy.gui.builder as tgb
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig = plt.figure(figsize =(5, 4))
+import taipy.gui.builder as tgb
+from taipy.gui import Gui
+
+fig = plt.figure(figsize=(5, 4))
 xx = np.arange(0, 2 * np.pi, 0.01)
-plot = fig.subplots(1,1)
-plot.fill(xx, np.sin(xx), facecolor='none', edgecolor='purple', linewidth=2)
+plot = fig.subplots(1, 1)
+plot.fill(xx, np.sin(xx), facecolor="none", edgecolor="purple", linewidth=2)
 
 # Create a Taipy Page
 with tgb.Page() as page:
