1cfb1ea9a56dbeeb37d2055f9c8ab74eda043445
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2024-10-10 08:47:21 +0200

    wait for the timer to execute

--- tests/gui/hooks/test_listener.py
+++ tests/gui/hooks/test_listener.py
@@ -8,6 +8,7 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
+import time
 import typing as t
 from unittest.mock import MagicMock
 
@@ -60,4 +61,5 @@ def test_listener(gui: Gui):
 
     gui._fire_event(event, None, payload)
 
+    time.sleep(0.3)
     listener.assert_called_once_with(event, None, payload)
