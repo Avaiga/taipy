34b67ee764978fce4a38117e355b0e88ff9c1504
Krishna Sarathi Ghosh <krishnasarathishem@gmail.com>
2024-10-16 15:36:36 +0530

    update to modify file structure and remove obsolete code

--- taipy/_entrypoint.py
+++ taipy/_entrypoint.py
@@ -22,63 +22,13 @@ from taipy.core._cli._core_cli_factory import _CoreCLIFactory
 from taipy.core._entity._migrate_cli import _MigrateCLI
 from taipy.core._version._cli._version_cli_factory import _VersionCLIFactory
 from taipy.gui._gui_cli import _GuiCLI
-from taipy.core.config.rest_config import RestSection, configure_rest  
+from taipy.rest.rest_config import RestSection, configure_rest  
 from taipy.gui import Gui
 from taipy.rest import Rest
 from taipy.config import Config
+from taipy.core import Orchestrator
 from .version import _get_version
-from taipy.core.config.rest_config import configure_rest
 
-
-def _entrypoint():
-    # Add the current working directory to path to execute version command on FS repo
-    sys.path.append(os.path.normpath(os.getcwd()))
-
-    _TaipyParser._parser.add_argument(
-        "-v",
-        "--version",
-        action="store_true",
-        help="Print the current Taipy version and exit.",
-    )
-
-    if find_spec("taipy.enterprise"):
-        from taipy.enterprise._entrypoint import _entrypoint_initialize as _enterprise_entrypoint_initialize
-
-        _enterprise_entrypoint_initialize()
-
-    _core_cli = _CoreCLIFactory._build_cli()
-
-    _RunCLI.create_parser()
-    _GuiCLI.create_run_parser()
-    _core_cli.create_run_parser()
-
-    _VersionCLIFactory._build_cli().create_parser()
-    _CreateCLI.generate_template_map()
-    _CreateCLI.create_parser()
-    _MigrateCLI.create_parser()
-    _HelpCLI.create_parser()
-
-    if find_spec("taipy.enterprise"):
-        from taipy.enterprise._entrypoint import _entrypoint_handling as _enterprise_entrypoint_handling
-
-        _enterprise_entrypoint_handling()
-
-    args, _ = _TaipyParser._parser.parse_known_args()
-    if args.version:
-        print(f"Taipy {_get_version()}")  # noqa: T201
-        sys.exit(0)
-
-    _RunCLI.handle_command()
-    _HelpCLI.handle_command()
-    _VersionCLIFactory._build_cli().handle_command()
-    _MigrateCLI.handle_command()
-    _CreateCLI.handle_command()
-
-    _TaipyParser._remove_argument("help")
-    _TaipyParser._parser.print_help()
-
-
-import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Taipy server")
@@ -88,46 +38,33 @@ def parse_args():
     parser.add_argument("--rest-use-https", action="store_true", help="Use HTTPS for REST server")
     parser.add_argument("--rest-ssl-cert", type=str, help="Path to SSL certificate for REST server")
     parser.add_argument("--rest-ssl-key", type=str, help="Path to SSL key for REST server")
-    # GUI server arguments
-    parser.add_argument("--gui-port", type=int, help="Port to run the GUI server on")
-    parser.add_argument("--gui-host", type=str, help="Host to run the GUI server on")
-    parser.add_argument("--gui-use-https", action="store_true", help="Use HTTPS for GUI server")
-    parser.add_argument("--gui-ssl-cert", type=str, help="Path to SSL certificate for GUI server")
-    parser.add_argument("--gui-ssl-key", type=str, help="Path to SSL key for GUI server")
     return parser.parse_args()
 
 def main():
-    args = parse_args()
-
-    # Configure REST
-    rest_config = {
-        "port": args.rest_port or 5000,
-        "host": args.rest_host or "127.0.0.1",
-        "use_https": args.rest_use_https,
-        "ssl_cert": args.rest_ssl_cert,
-        "ssl_key": args.rest_ssl_key
-    }
-    rest = Rest(**rest_config)
-
-    # Configure GUI
-    gui_config = {
-        "port": args.gui_port or 5001,  # Note: different default port
-        "host": args.gui_host or "127.0.0.1",
-        "use_reloader": False,
-        "use_https": args.gui_use_https,
-        "ssl_certfile": args.gui_ssl_cert,
-        "ssl_keyfile": args.gui_ssl_key
-    }
     gui = Gui()
 
-    # Run the application with both GUI and REST
+    gui_args = _GuiCLI.handle_command()
+
     gui.run(
         run_server=True,
-        rest=rest,
-        **gui_config
+        **vars(gui_args)  
     )
 
 if __name__ == "__main__":
-    main()
+    # Initialize and run Taipy Core
+    orchestrator = Orchestrator()
+    orchestrator.run()
 
+    # Configure REST
+    rest_config = {
+        "port": 5000,
+        "host": "127.0.0.1",
+        "use_https": False,
+        "ssl_cert": None,
+        "ssl_key": None
+    }
+    rest = Rest()
+
+    # Run the REST service
+    rest.run(**rest_config)
 
--- taipy/rest/rest_config.py
+++ taipy/rest/rest_config.py
@@ -2,21 +2,19 @@ from taipy.config import Config, Section, _inject_section
 from typing import Optional
 
 class RestSection(Section):
+    @property
+    def name(self) -> str:
+        return "REST"
+
     def __init__(self):
-        super().__init__(id="rest") 
         self._port: int = 5000
         self._host: str = "127.0.0.1"
         self._use_https: bool = False
         self._ssl_cert: Optional[str] = None
         self._ssl_key: Optional[str] = None
 
-
     @property
-    def name(self) -> str:
-        return "rest"
-
-    @property
-    def port(self):
+    def port(self) -> int:
         return self._port
 
     @port.setter
@@ -24,7 +22,7 @@ class RestSection(Section):
         self._port = value
 
     @property
-    def host(self):
+    def host(self) -> str:
         return self._host
 
     @host.setter
@@ -32,7 +30,7 @@ class RestSection(Section):
         self._host = value
 
     @property
-    def use_https(self):
+    def use_https(self) -> bool:
         return self._use_https
 
     @use_https.setter
@@ -55,25 +53,14 @@ class RestSection(Section):
     def ssl_key(self, value: Optional[str]):
         self._ssl_key = value
 
-
-default_rest_section = RestSection()
-
-def configure_rest(port: int = 5000, 
-                   host: str = "127.0.0.1", 
-                   use_https: bool = False, 
-                   ssl_cert: Optional[str] = None, 
-                   ssl_key: Optional[str] = None):
-    if use_https:
-        if ssl_cert is None or ssl_key is None:
-            raise ValueError("SSL certificate and key are required when use_https is True")
-    config = RestSection()
+def configure_rest(port: int = 5000, host: str = "127.0.0.1", use_https: bool = False,
+                   ssl_cert: Optional[str] = None, ssl_key: Optional[str] = None):
+    config = Config.rest
     config.port = port
     config.host = host
     config.use_https = use_https
     config.ssl_cert = ssl_cert
     config.ssl_key = ssl_key
-    Config._register(config)
 
-configuration_methods = [("configure_rest", configure_rest)]
+_inject_section("rest", "rest", default=RestSection(), configuration_methods=[("configure_rest", configure_rest)])
 
-_inject_section(RestSection, "rest", default_rest_section, configuration_methods)
