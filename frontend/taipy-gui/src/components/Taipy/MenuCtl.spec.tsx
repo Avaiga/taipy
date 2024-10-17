e89e7264a33fde675397b89b9358643e72435e2e
namnguyen <namnguyen20999@gmail.com>
2024-10-17 09:37:57 +0700

    using createSendUpdateAction to update selected

--- frontend/taipy-gui/src/components/Taipy/MenuCtl.spec.tsx
+++ frontend/taipy-gui/src/components/Taipy/MenuCtl.spec.tsx
@@ -63,6 +63,9 @@ describe("MenuCtl Component", () => {
                     className: "",
                     inactiveIds: ["id1", "id2"],
                     label: "MenuCtl",
+                    onChange: undefined,
+                    selected: [],
+                    selectedUpdateVars: "",
                     lov: [
                         {
                             children: [],
@@ -87,7 +90,6 @@ describe("MenuCtl Component", () => {
                     ],
                     onAction: "on_action",
                     width: "15vw",
-                    selectedIds: [],
                 },
                 type: "SET_MENU",
             });
