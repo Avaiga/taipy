6d1d5f2d016343ae09837788ebdc1523cdb1d710
Anuj Kumar Sharma <anujkumsharma9876@gmail.com>
2024-10-17 12:13:17 +0000

    fixing linter and spell error

--- doc/gui/examples/controls/column_name_styling.py
+++ doc/gui/examples/controls/column_name_styling.py
@@ -1,8 +1,7 @@
 # Example for column name styling for header
 
-from taipy.gui import Gui
+from taipy.gui import Gui, Markdown
 import pandas as pd
-from taipy.gui import Markdown
 
 # Sample data in DataFrame format
 df = pd.DataFrame({
@@ -15,7 +14,7 @@ df = pd.DataFrame({
 # Page content with table and header styling
 page = Markdown("""
 <|table|data={df}|columns={columns}|>
-""", sytle={".taipy-table-name": {"color": "blue"}, ".taipy-table-job-or-occupation": {"color": "green"}})
+""", style={".taipy-table-name": {"color": "blue"}, ".taipy-table-job-or-occupation": {"color": "green"}})
 
 if __name__ == "__main__":
     Gui(page).run(title="Column Name Styling Example")
