7fc9c4cb4943d2bbb1db016639272c6c94635964
prakharjain <prakharjain2004@gmail.com>
2024-10-22 00:10:26 +0530

    Fix: Enforce editor lock for write, append, and upload in DataNode.

--- taipy/core/data/in_memory.py
+++ taipy/core/data/in_memory.py
@@ -13,7 +13,7 @@ from datetime import datetime, timedelta
 from typing import Any, Dict, List, Optional, Set
 
 from taipy.common.config.common.scope import Scope
-
+from taipy.core.exceptions.exceptions import DataNodeIsBeingEdited
 from .._version._version_manager_factory import _VersionManagerFactory
 from .data_node import DataNode
 from .data_node_id import DataNodeId, Edit
@@ -98,3 +98,16 @@ class InMemoryDataNode(DataNode):
 
     def _write(self, data):
         in_memory_storage[self.id] = data
+
+    def _append(self, data):
+        """Append data to the existing data in the in-memory storage."""
+        if self.id not in in_memory_storage:
+            in_memory_storage[self.id] = []
+
+        in_memory_storage[self.id].append(data)  # Append new data
+
+    def _upload(self, data, editor_id: Optional[str] = None):
+        """Simulate uploading data with editor locking enforcement."""
+        if self.edit_in_progress and self.editor_id != editor_id:
+            raise DataNodeIsBeingEdited(self.id, self.editor_id)
+        self._write(data)
