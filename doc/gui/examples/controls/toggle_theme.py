aacad32c7ff460afd804935ba2c4fd3fc2991e62
Fabien Lelaquais <fabien.lelaquais@avaiga.com>
2025-03-06 18:23:15 +0100

    Wrong binding

--- doc/gui/examples/controls/toggle_theme.py
+++ doc/gui/examples/controls/toggle_theme.py
@@ -15,14 +15,14 @@
 # -----------------------------------------------------------------------------------------
 from taipy.gui import Gui
 
-dark_theme = ""
+dark_theme = True
 
 
 def on_change(state):
     print(f"Dark theme set: {state.dark_theme}")
 
 
-page = "<|{theme}|toggle|theme|>"
+page = "<|{dark_theme}|toggle|theme|>"
 
 if __name__ == "__main__":
     Gui(page).run(title="Toggle - Theme")
