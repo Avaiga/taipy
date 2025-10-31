b2c087cd8e1808df642427545303186018627d6d
Agney <agneynalapat123@gmail.com>
2025-10-31 12:29:27 +0530

    max-bounds-free-type-fixwa

--- frontend/taipy-gui/src/components/Taipy/Input.tsx
+++ frontend/taipy-gui/src/components/Taipy/Input.tsx
@@ -168,7 +168,7 @@ const Input = (props: TaipyInputProps) => {
                     : multiline
                     ? evt.currentTarget.value
                     : evt.currentTarget.value;
-            // Always dispatch on blur within the delay
+            // Always dispatch on blur within the time delay
             if (delayCall.current > 0 || changeDelay === -1 || delayCall.current === -1) {
                 if (changeDelay > 0) {
                     clearTimeout(delayCall.current);
