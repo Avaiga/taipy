fef6b5d3c0c5715026dca97a987ec083d17f2234
JeanRobin <jeanrobin.medori@taipy.io>
2026-02-05 15:25:42 +0100

    2nd attempt

--- taipy/core/_core.py
+++ taipy/core/_core.py
@@ -23,7 +23,7 @@ class Core:
     def __new__(cls) -> Orchestrator:  # type: ignore
         _warn_deprecated("'Core'", suggest="the 'Orchestrator' class")
         cls.__logger.warning(
-            "The `Core` service is deprecated and replaced by the `Orchestrator` service."
+            "The `Core` service is deprecated and replaced by the `Orchestrator` service. "
             "An `Orchestrator` instance has been instantiated instead."
         )
         return Orchestrator()
