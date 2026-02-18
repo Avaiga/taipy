2ef4d8e40cf4011741fbf4dc5a8060b48c92970f
JeanRobin <jeanrobin.medori@taipy.io>
2026-02-18 20:15:34 +0100

    Attempt to fix Git path on clean up action: token

--- tools/release/cleanup_dev_releases.py
+++ tools/release/cleanup_dev_releases.py
@@ -13,6 +13,7 @@
 # --------------------------------------------------------------------------------------------------
 
 import argparse
+import os
 
 import requests
 from common import Git, Version, fetch_github_releases
@@ -48,10 +49,11 @@ The indicated version must not have extensions.""",
 
     args = parser.parse_args(arg_strings)
     version = args.version
+    extension = args.extension
+    token = os.environ.get("GITHUB_TOKEN")
     dry_run = args.dry_run.strip().lower() in ("true", "1", "yes")
     if dry_run:
         print("⚠️  DRY RUN MODE ENABLED ⚠️")  # noqa: T201
-    extension = args.extension
 
     github_path = Git.get_github_path()
     all_releases = fetch_github_releases(github_path)
@@ -69,17 +71,17 @@ The indicated version must not have extensions.""",
                     release_tag = release["tag"]
                     if version.matches(release_version) and release_version.validate_extension(ext=extension):
 
-                        if not __delete_release(dry_run, github_path, package.name, release_id, release_version):
+                        if not __delete_release(dry_run, github_path, token, package.name, release_id, release_version):
                             errors.append(f"Release {release_version}-{package.name} (id: {release_id})")
                         else:
                             del_releases.append(release_id)
-                        if not __delete_tag(dry_run, github_path, release_tag):
+                        if not __delete_tag(dry_run, github_path, token, release_tag):
                             errors.append(f"Tag {release_tag}")
                         else:
                             del_tags.append(release_tag)
                     elif not version.matches(release_version) and release_version.validate_extension(ext=extension):
                         found_wrong_version.append(release_tag)
-                    elif version.matches(release_version) :
+                    elif version.matches(release_version):
                         found_wrong_extension.append(release_tag)
 
     beautiful_traces(version, extension, del_releases, del_tags, errors, found_wrong_extension, found_wrong_version)
@@ -90,47 +92,53 @@ def beautiful_traces(version, extension, del_releases, del_tags, errors, found_w
     if len(del_releases) == 0:
         print(f"No dev releases deleted for version {version}.")  # noqa: T201
     else:
-        print(
-            f"✅ Successfully deleted {len(del_releases)} releases {version} with extension '{extension}'")  # noqa: T201
+        print(  # noqa: T201
+            f"✅ Successfully deleted {len(del_releases)} releases {version} " +  # noqa: T201
+            f"with extension '{extension}'")  # noqa: T201
     if len(del_tags) == 0:
         print(f"No tags deleted for version {version} with extension '{extension}'.")  # noqa: T201
     else:
-        print(
-            f"✅ Successfully deleted {len(del_tags)} tags for version {version} with extension '{extension}'")  # noqa: T201
+        print(  # noqa: T201
+            f"✅ Successfully deleted {len(del_tags)} tags for version " +  # noqa: T201
+            f"{version} with extension '{extension}'")  # noqa: T201
     if len(errors) > 0:
         print(f"❌ Failed to delete {len(errors)} items.")  # noqa: T201
-    print(" ")
+    print(" ") # noqa: T201
     if len(found_wrong_extension) > 0:
-        print(
-            f"Found {len(found_wrong_extension)} releases matching version {version} but with another extension:")  # noqa: T201
+        print(  # noqa: T201
+            f"Found {len(found_wrong_extension)} releases matching version " +  # noqa: T201
+            f"{version} but with another extension:")  # noqa: T201
         print(sorted(found_wrong_extension))  # noqa: T201
     if len(found_wrong_version) > 0:
-        print(
-            f"Found {len(found_wrong_version)} releases matching extension '{extension}' but with another version:")  # noqa: T201
+        print(  # noqa: T201
+            f"Found {len(found_wrong_version)} releases matching extension " +  # noqa: T201
+            f"'{extension}' but with another version:")  # noqa: T201
         print(sorted(found_wrong_version))  # noqa: T201
 
 
-def __delete_release(dry_run, github_path: str, pkg: str, release_id: str, release_version: Version) -> bool:
+def __delete_release(dry_run, github_path: str, token: str, pkg: str, release_id: str, rel_version: Version) -> bool:
     url = f"https://api.github.com/repos/{github_path}/releases/{release_id}"
     if dry_run:
-        print(f'requests.delete("{url}", ' + # noqa: T201
-              'headers={"Accept": "application/vnd.github+json"})')  # noqa: T201
+        print(f'requests.delete("{url}", ' +  # noqa: T201
+              'headers={"Accept": "application/vnd.github+json", "Authorization": "Bearer ' + token +  # noqa: T201
+              '"})')  # noqa: T201
         return True
     else:
         resp = requests.delete(url, headers={"Accept": "application/vnd.github+json"})
         if resp.status_code == 204:
-            print(f"Successfully deleted '{pkg}-{release_version}'.")  # noqa: T201
+            print(f"Successfully deleted '{pkg}-{rel_version}'.")  # noqa: T201
             return True
         else:
-            print(f"❌ Failed deleting '{pkg}-{release_version}': {resp.status_code} - {resp.text}")  # noqa: T201
+            print(f"❌ Failed deleting '{pkg}-{rel_version}': {resp.status_code} - {resp.text}")  # noqa: T201
             return False
 
 
-def __delete_tag(dry_run, github_path: str, release_tag: str) -> bool:
+def __delete_tag(dry_run, github_path: str, token: str, release_tag: str) -> bool:
     url = f"https://api.github.com/repos/{github_path}/git/refs/tags/{release_tag}"
     if dry_run:
         print(f'requests.delete("{url}", ' +  # noqa: T201
-              'headers={"Accept": "application/vnd.github+json"})')  # noqa: T201
+              'headers={"Accept": "application/vnd.github+json", "Authorization": "Bearer ' + token +  # noqa: T201
+              '"})')  # noqa: T201
         return True
     else:
         resp = requests.delete(url, headers={"Accept": "application/vnd.github+json"})
