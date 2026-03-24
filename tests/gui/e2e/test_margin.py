d103b5e41985bc0df351eb93c1d0053c5437c62d
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2026-03-24 16:57:11 +0100

    remove warning in rest datanode try the make the test work like on desktop

--- tests/gui/e2e/test_margin.py
+++ tests/gui/e2e/test_margin.py
@@ -30,12 +30,9 @@ def test_margin_1(page: "Page", gui: Gui, helpers):
     helpers.run_e2e(gui, dark_mode=False, margin="10rem")
     page.goto("./")
     page.expect_websocket()
-    style = page.locator('style[data-href="taipy-style"]')
-    style.wait_for(state="attached")
+    page.locator('style[data-href="taipy-style"]').wait_for(state="attached")
     page.locator("#text1").wait_for()
-    rootDiv = page.locator("#root")
-    rootDiv.wait_for()
-    margin = rootDiv.evaluate("el => getComputedStyle(el).getPropertyValue('margin')")
+    margin = page.locator("#root").evaluate("el => getComputedStyle(el).getPropertyValue('margin')")
     assert margin == "160px"
 
 
@@ -49,12 +46,9 @@ def test_margin_2(page: "Page", gui: Gui, helpers):
     helpers.run_e2e(gui, dark_mode=False)
     page.goto("./")
     page.expect_websocket()
-    style = page.locator('style[data-href="taipy-style"]')
-    style.wait_for(state="attached")
+    page.locator('style[data-href="taipy-style"]').wait_for(state="attached")
     page.locator("#text1").wait_for()
-    rootDiv = page.locator("#root")
-    rootDiv.wait_for()
-    margin = rootDiv.evaluate("el => getComputedStyle(el).getPropertyValue('margin')")
+    margin = page.locator("#root").evaluate("el => getComputedStyle(el).getPropertyValue('margin')")
     assert margin == "16px"
 
 
@@ -68,12 +62,12 @@ def test_margin_3(page: "Page", gui: Gui, helpers):
     helpers.run_e2e(gui, dark_mode=False, margin="10rem", stylekit=True)
     page.goto("./")
     page.expect_websocket()
-    style = page.locator('style[data-href="taipy-style"]')
-    style.wait_for(state="attached")
+    page.locator('style[data-href="taipy-style"]').wait_for(state="attached")
     page.locator("#text1").wait_for()
-    rootDiv = page.locator("#root")
-    rootDiv.wait_for()
-    margin = rootDiv.evaluate("el => getComputedStyle(el).getPropertyValue('margin')")
+    margin = page.locator("#root").evaluate("el => getComputedStyle(el).getPropertyValue('margin')")
+    if margin != "160px":
+        page.wait_for_timeout(1000)
+        margin = page.locator("#root").evaluate("el => getComputedStyle(el).getPropertyValue('margin')")
     assert margin == "160px"
 
 
@@ -87,12 +81,12 @@ def test_margin_4(page: "Page", gui: Gui, helpers):
     helpers.run_e2e(gui, dark_mode=False, stylekit={"root_margin": "20rem"})
     page.goto("./")
     page.expect_websocket()
-    style = page.locator('style[data-href="taipy-style"]')
-    style.wait_for(state="attached")
+    page.locator('style[data-href="taipy-style"]').wait_for(state="attached")
     page.locator("#text1").wait_for()
-    rootDiv = page.locator("#root")
-    rootDiv.wait_for()
-    margin = rootDiv.evaluate("el => getComputedStyle(el).getPropertyValue('margin')")
+    margin = page.locator("#root").evaluate("el => getComputedStyle(el).getPropertyValue('margin')")
+    if margin != "320px":
+        page.wait_for_timeout(1000)
+        margin = page.locator("#root").evaluate("el => getComputedStyle(el).getPropertyValue('margin')")
     assert margin == "320px"
 
 
@@ -106,10 +100,10 @@ def test_margin_5(page: "Page", gui: Gui, helpers):
     helpers.run_e2e(gui, dark_mode=False, stylekit={"root_margin": "20rem"}, margin="10rem")
     page.goto("./")
     page.expect_websocket()
-    style = page.locator('style[data-href="taipy-style"]')
-    style.wait_for(state="attached")
+    page.locator('style[data-href="taipy-style"]').wait_for(state="attached")
     page.locator("#text1").wait_for()
-    rootDiv = page.locator("#root")
-    rootDiv.wait_for()
-    margin = rootDiv.evaluate("el => getComputedStyle(el).getPropertyValue('margin')")
+    margin = page.locator("#root").evaluate("el => getComputedStyle(el).getPropertyValue('margin')")
+    if margin != "320px":
+        page.wait_for_timeout(1000)
+        margin = page.locator("#root").evaluate("el => getComputedStyle(el).getPropertyValue('margin')")
     assert margin == "320px"
