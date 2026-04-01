640f8e4a6420a3bf4cdca8887bfa649f40b73679
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2026-04-01 20:14:14 +0200

    prefix params with _

--- frontend/taipy-gui/src/components/Taipy/tableUtils.spec.tsx
+++ frontend/taipy-gui/src/components/Taipy/tableUtils.spec.tsx
@@ -76,8 +76,7 @@ describe("Editable cell", () => {
         });
     });
     describe("Editable mode", () => {
-        // @ts-ignore
-        const onValidation = (value: RowValue, rowIndex: number, colName: string, userValue: string, tz?: string) => {};
+        const onValidation = (_value: RowValue, _rowIndex: number, _colName: string, _userValue: string, _tz?: string) => {};
         it("show a boolean as a switch", async () => {
             const { getByRole, getByTestId } = render(
                 <EditableCell
